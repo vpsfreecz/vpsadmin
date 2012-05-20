77286fb2022120e4c3d65832b58a97612a00a22c
Jakub Skokan <jakub.skokan@havefun.cz>
2012-05-20 12:06:20 +0200

    Fixed check_daemon.sh

--- check_daemon.sh
+++ check_daemon.sh
@@ -4,6 +4,6 @@ PID="`cat /var/run/vpsadmin.pid`"
 if [ ! -f /proc/$PID/status ]; then
 	rm /var/run/vpsadmin.pid
 	php /opt/vpsadmin/daemon.php
-	echo "`hostname`: daemon dead, starting." | mail -s "`hostname`: starting vpsAdmin daemon" --from vpsadmin@vpsfree.cz snajpa@snajpa.net
+	echo "`hostname`: daemon dead, starting." | mail -s "`hostname`: starting vpsAdmin daemon" snajpa@snajpa.net -- -from vpsadmin@vpsfree.cz
 fi
 
