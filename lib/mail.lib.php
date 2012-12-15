ab724434dc0679901b9e3d69c189831f2771e4e7
Jakub Skokan <jakub.skokan@havefun.cz>
2012-12-16 00:09:32 +0100

    Fix mailer server select

--- lib/mail.lib.php
+++ lib/mail.lib.php
@@ -2,8 +2,9 @@
 function send_mail($to, $subject, $msg, $cc = array(), $bcc = array(), $html = false, $dep = NULL) {
 	global $cluster;
 	$mailers = $cluster->list_servers_with_type("mailer");
+	$ids = arrays_keys($mailers);
 	
-	add_transaction($_SESSION["member"]["m_id"], $mailers[0], 0, T_MAIL_SEND, array(
+	add_transaction($_SESSION["member"]["m_id"], $ids[0], 0, T_MAIL_SEND, array(
 		"to" => $to,
 		"subject" => $subject,
 		"msg" => $msg,
