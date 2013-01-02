2366bae210591350a7c3d0315403b7d8a0af4332
Jakub Skokan <jakub.skokan@havefun.cz>
2013-01-02 21:13:27 +0100

    Fix typo in mailer

--- lib/mail.lib.php
+++ lib/mail.lib.php
@@ -2,7 +2,7 @@
 function send_mail($to, $subject, $msg, $cc = array(), $bcc = array(), $html = false, $dep = NULL) {
 	global $cluster;
 	$mailers = $cluster->list_servers_with_type("mailer");
-	$ids = arrays_keys($mailers);
+	$ids = array_keys($mailers);
 	
 	add_transaction($_SESSION["member"]["m_id"], $ids[0], 0, T_MAIL_SEND, array(
 		"to" => $to,
