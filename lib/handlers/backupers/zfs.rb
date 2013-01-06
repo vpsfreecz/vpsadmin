2b36919da22e2e759eefac8625f5fa04885fcde8
Jakub Skokan <jakub.skokan@havefun.cz>
2013-01-06 22:09:57 +0100

    ZFS backuper: fix paths

--- lib/handlers/backupers/zfs.rb
+++ lib/handlers/backupers/zfs.rb
@@ -16,7 +16,7 @@ module BackuperBackend
 			f.close
 			
 			syscmd("#{$APP_CONFIG[:bin][:zfs]} create -p #{$APP_CONFIG[:backuper][:zfs][:zpool]}/#{@veid}")
-			syscmd("#{$APP_CONFIG[:bin][:rsync]} -rlptgoDHX --numeric-ids --inplace --delete-after --exclude-from #{f.path} #{mountpoint} #{$APP_CONFIG[:backuper][:dest]}/#{@veid}")
+			syscmd("#{$APP_CONFIG[:bin][:rsync]} -rlptgoDHX --numeric-ids --inplace --delete-after --exclude-from #{f.path} #{mountpoint}/ #{$APP_CONFIG[:backuper][:dest]}/#{@veid}/")
 			syscmd("#{$APP_CONFIG[:bin][:zfs]} snapshot #{$APP_CONFIG[:backuper][:zfs][:zpool]}/#{@veid}@#{Time.new.to_i}")
 			
 			db.prepared("DELETE FROM vps_backups WHERE vps_id = ?", @veid)
