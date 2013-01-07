44bd07a1cbffb134784ff585ca438bdb5b647493
Jakub Skokan <jakub.skokan@havefun.cz>
2013-01-07 11:08:04 +0100

    ZFS backuper: fix snapshot list parsing and accept rsync exit code 24

--- lib/handlers/backupers/zfs.rb
+++ lib/handlers/backupers/zfs.rb
@@ -16,13 +16,13 @@ module BackuperBackend
 			f.close
 			
 			syscmd("#{$APP_CONFIG[:bin][:zfs]} create -p #{$APP_CONFIG[:backuper][:zfs][:zpool]}/#{@veid}")
-			syscmd("#{$APP_CONFIG[:bin][:rsync]} -rlptgoDHX --numeric-ids --inplace --delete-after --exclude-from #{f.path} #{mountpoint}/ #{$APP_CONFIG[:backuper][:dest]}/#{@veid}/")
+			syscmd("#{$APP_CONFIG[:bin][:rsync]} -rlptgoDHX --numeric-ids --inplace --delete-after --exclude-from #{f.path} #{mountpoint}/ #{$APP_CONFIG[:backuper][:dest]}/#{@veid}/", [24,])
 			syscmd("#{$APP_CONFIG[:bin][:zfs]} snapshot #{$APP_CONFIG[:backuper][:zfs][:zpool]}/#{@veid}@#{Time.new.strftime("%Y-%m-%dT%H:%M:%S")}")
 			
 			db.prepared("DELETE FROM vps_backups WHERE vps_id = ?", @veid)
 			
 			syscmd("#{$APP_CONFIG[:bin][:zfs]} list -r -t snapshot -H -o name #{$APP_CONFIG[:backuper][:zfs][:zpool]}/#{@veid}")[:output].split('\n').each do |snapshot|
-				db.prepared("INSERT INTO vps_backups SET vps_id = ?, timestamp = UNIX_TIMESTAMP(?)", @veid, snapshot)
+				db.prepared("INSERT INTO vps_backups SET vps_id = ?, timestamp = UNIX_TIMESTAMP(?)", @veid, snapshot.split("@")[1])
 			end
 			
 			db.close
