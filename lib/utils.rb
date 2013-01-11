eec7d1cbb22b1a62a0037cedfbd15d3ca2743e27
Jakub Skokan <jakub.skokan@havefun.cz>
2013-01-11 19:50:47 +0100

    Improve duration formatting

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,6 +1,12 @@
 def format_duration(interval)
+	d = interval / 86400
 	h = interval / 3600 % 24
 	m = interval / 60 % 60
 	s = interval % 60
-	"%02d:%02d:%02d" % [h, m, s]
+	
+	if d > 0
+		"%d days, %02d:%02d:%02d" % [d, h, m, s]
+	else
+		"%02d:%02d:%02d" % [h, m, s]
+	end
 end
