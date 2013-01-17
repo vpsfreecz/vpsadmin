f292654e7213be81726fda5f9e6677c4b3082651
Jakub Skokan <jakub.skokan@havefun.cz>
2013-01-17 10:03:47 +0100

    Sort workers and consoles list, allow listing them at the same time

--- lib/rc.rb
+++ lib/rc.rb
@@ -2,7 +2,7 @@ require 'lib/vpsadmind'
 require 'lib/utils'
 
 module VpsAdminCtl
-	VERSION = "1.5.5"
+	VERSION = "1.5.6"
 	ACTIONS = [:status, :reload, :stop, :restart, :update]
 	
 	class RemoteControl
@@ -14,16 +14,20 @@ module VpsAdminCtl
 			if @opts[:workers]
 				puts sprintf("%-5s %-20.15s %-5s %-18.16s %s", "VEID", "HANDLER", "TYPE", "TIME", "STEP") if @opts[:header]
 				
-				@res["workers"].each do |wid, w|
-					puts sprintf("%-5d %-20.15s %-5d %-18.16s %s", wid, w["handler"], w["type"], format_duration(Time.new.to_i - w["start"]), w["step"])
+				@res["workers"].sort.each do |w|
+					puts sprintf("%-5d %-20.15s %-5d %-18.16s %s", w[0], w[1]["handler"], w[1]["type"], format_duration(Time.new.to_i - w[1]["start"]), w[1]["step"])
 				end
-			elsif @opts[:consoles]
+			end
+			
+			if @opts[:consoles]
 				puts sprintf("%-5s %s", "VEID", "LISTENERS")  if @opts[:header]
 				
-				@res["consoles"].each do |veid, usage|
-					puts sprintf("%-5d %d", veid, usage)
+				@res["consoles"].sort.each do |c|
+					puts sprintf("%-5d %d", c[0], c[1])
 				end
-			else
+			end
+			
+			unless @opts[:workers] || @opts[:consoles]
 				puts "Version: #{@vpsadmind.version}"
 				puts "Uptime: #{format_duration(Time.new.to_i - @res["start_time"])}"
 				puts "Workers: #{@res["workers"].size}/#{@res["threads"]}"
