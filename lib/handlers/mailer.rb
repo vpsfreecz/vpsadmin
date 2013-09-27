4b25f0d26371b2771ec95b4d897c572d38ef4f10
Jakub Skokan <jakub.skokan@havefun.cz>
2013-09-27 20:26:23 +0200

    Mailer: fix message-id and in-reply-to checking

--- lib/handlers/mailer.rb
+++ lib/handlers/mailer.rb
@@ -16,8 +16,8 @@ Subject: #{@params["subject"]}
 HEADERS
 
 		headers += "CC: #{@params["cc"].join(",")}\n" if @params["cc"].count > 0
-		headers += "Message-ID: <#{@params["msg_id"]}>\n" unless @params["msg_id"].empty?
-		headers += "In-Reply-To: <#{@params["in_reply_to"]}>\n" unless @params["in_reply_to"].empty?
+		headers += "Message-ID: <#{@params["msg_id"]}>\n" unless @params["msg_id"]
+		headers += "In-Reply-To: <#{@params["in_reply_to"]}>\n" unless @params["in_reply_to"]
 		headers += "References: <#{@params["references"].join("\n  ")}>\n" unless @params["references"].empty?
 
 		msg = <<MSG
