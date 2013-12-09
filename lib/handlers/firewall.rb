556e4b4f51aebb44fc08ef31e6ffcba211da6f38
Jakub Skokan <jakub.skokan@havefun.cz>
2013-12-09 19:12:49 +0100

    iptables: handle resource temporarily unavailable error

--- lib/handlers/firewall.rb
+++ lib/handlers/firewall.rb
@@ -132,7 +132,27 @@ class Firewall < Executor
 			options << opts
 		end
 		
-		syscmd("#{$CFG.get(:bin, ver == 4 ? :iptables : :ip6tables)} #{options.join(" ")}", valid_rcs)
+		try_cnt = 0
+		
+		begin
+			syscmd("#{$CFG.get(:bin, ver == 4 ? :iptables : :ip6tables)} #{options.join(" ")}", valid_rcs)
+		
+		rescue CommandFailed => err
+			if err.rc == 1 && err.output =~ /Resource temporarily unavailable/
+				if try_cnt == 3
+					log "Run out of tries"
+					raise err
+				end
+				
+				log "#{err.cmd} failed with error 'Resource temporarily unavailable', retrying in 3 seconds"
+				
+				try_cnt += 1
+				sleep(3)
+				retry
+			else
+				raise err
+			end
+		end
 	end
 	
 	def Firewall.mutex
