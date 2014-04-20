8daa2c6cdf3d4693230dc34208410d342136f19b
Jakub Skokan <jakub.skokan@havefun.cz>
2014-04-20 09:38:07 +0200

    update: add option --force

--- lib/commands/update.rb
+++ lib/commands/update.rb
@@ -1,6 +1,20 @@
 module Commands
-  class Stop < Command
+  class Update < Command
     description 'Safely stop vpsAdmind, then update by git pull and start again'
+
+    def options(opts, args)
+      @opts = {
+          :force => false,
+      }
+
+      opts.on('-f', '--force', 'Force update - kills all transactions that are being processed and updates immediately') do
+        @opts[:force] = true
+      end
+    end
+
+    def prepare
+      @opts
+    end
 
     def process
       puts 'Update scheduled'
