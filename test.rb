1135d8ae867b490f96847976829ec7850ecb153e
Jakub Skokan <jakub.skokan@havefun.cz>
2014-05-15 19:38:53 +0200

    Test script: remove devel load path

--- test.rb
+++ test.rb
@@ -1,31 +1,8 @@
-$: << '/home/aither/workspace/vpsfree.cz/dev1.orion/haveapi/lib'
-
 require './lib/vpsadmin/vpsadmin'
-
-#VpsAdmin::API.get_version(:all).each do |r|
-#  #puts "Resource #{r}"
-#  #r.actions do |a|
-#  #  puts "\tAction #{a}: desc=#{a.desc}"
-#  #end
-#
-#
-#  r.routes.each do |r|
-#    puts "#{r.http_method.to_s.upcase} #{r.url} - #{r.description}"
-#  end
-#end
 
 api = HaveAPI::Server.new(VpsAdmin::API::Resources)
 api.use_version([1, 2])
 api.set_default_version(1)
 api.mount('/')
 
-#v1 = VpsAdmin::API.get_version(1)
-#VpsAdmin::API.mount('/v1/', v1)
-
-# VpsAdmin::API::App.routes.each do |http_method, routes|
-#   routes.each do |route|
-#     puts "#{http_method} #{route}"
-#   end
-# end
 api.start!
-
