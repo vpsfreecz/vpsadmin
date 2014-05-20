1daf16333b4e73239a03c161f97d65c2cf4f5c73
Jakub Skokan <jakub.skokan@havefun.cz>
2014-05-20 20:21:05 +0200

    Vps.create: fix DNS resolver setting
    Same problem as for Vps.reinstall, transaction param DNS resolvers
    must be an Array.

--- models/transactions/vps/create.rb
+++ models/transactions/vps/create.rb
@@ -11,7 +11,7 @@ module Transactions::Vps
           hostname: vps.hostname,
           template: vps.os_template.name,
           onboot: vps.node.location.location_vps_onboot,
-          nameserver: vps.dns_resolver.addr
+          nameserver: vps.dns_resolver.addr.split(',')
       }
     end
   end
