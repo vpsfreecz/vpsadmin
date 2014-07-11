838c9e1698b8cb022a9ab99f9e1d6a6da91f84a9
Jakub Skokan <jakub.skokan@havefun.cz>
2014-07-11 14:29:05 +0200

    Move models to standalone repository

--- models/api_token.rb
+++
@@ -1,6 +0,0 @@
-class ApiToken < ActiveRecord::Base
-  belongs_to :user
-
-  validates :user_id, :token, presence: true
-  validates :token, length: {is: 100}
-end
--- models/environment_os_template.rb
+++
@@ -1,5 +0,0 @@
-class EnvironmentOsTemplate < ActiveRecord::Base
-  belongs_to :environment
-  belongs_to :os_template
-  has_paper_trail
-end
