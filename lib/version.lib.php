b5a3975df8fedc515e0fad3d3f440b15b72c9be1
Jakub Skokan <jakub.skokan@havefun.cz>
2014-10-16 10:08:04 +0200

    Version 1.22.3

--- lib/version.lib.php
+++ lib/version.lib.php
@@ -5,7 +5,7 @@
     Copyright (C) 2008-2011 Pavel Snajdr, snajpa@snajpa.net
 */
 
-define("VERSION", '1.22.2');
+define("VERSION", '1.22.3');
 define("DEMO_MODE", false);
 
 define("DB_VERSION", 15);
