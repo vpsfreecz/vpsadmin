4fe14a77cbc453e46257f6637fa9ccf9782cc983
Jakub Skokan <jakub.skokan@havefun.cz>
2014-10-18 19:05:42 +0200

    Command template ResourceControl: ruby 1.8.7 compatibility

--- lib/vpsadmindctl/command_templates/resource_control.rb
+++ lib/vpsadmindctl/command_templates/resource_control.rb
@@ -6,7 +6,7 @@ module VpsAdmindCtl::CommandTemplates
       raise ValidationError.new('missing resource') if @args.size < 2
       raise ValidationError.new('not a valid resource') unless %w(all fw shaper).include?(@args[1])
 
-      {resources: @args[1] == 'all' ? %i(fw shaper) : [@args[1]]}
+      {:resources => @args[1] == 'all' ? [:fw, :shaper] : [@args[1]]}
     end
 
     def process
