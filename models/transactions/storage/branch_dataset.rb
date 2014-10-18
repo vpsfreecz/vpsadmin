9ba856bb5ff50f673261196e1f1b0c675ab6d5cb
Jakub Skokan <jakub.skokan@havefun.cz>
2014-10-18 15:03:50 +0200

    Dataset branch: support both create from scratch and from snapshot

--- models/transactions/storage/branch_dataset.rb
+++ models/transactions/storage/branch_dataset.rb
@@ -3,16 +3,21 @@ module Transactions::Storage
     t_name :storage_create_branch_dataset
     t_type 5206
 
-    def params(branch, src_snapshot_in_branch)
+    def params(branch, src_snapshot_in_branch = nil)
       self.t_server = branch.dataset_in_pool.pool.node_id
 
-      {
+      ret = {
           pool_fs: branch.dataset_in_pool.pool.filesystem,
           dataset_name: branch.dataset_in_pool.dataset.full_name,
           new_branch_name: branch.full_name,
-          from_branch_name: src_snapshot_in_branch.branch.full_name,
-          from_snapshot: src_snapshot_in_branch.snapshot.name
       }
+
+      if src_snapshot_in_branch
+        ret[:from_branch_name] = src_snapshot_in_branch.branch.full_name,
+        ret[:from_snapshot] = src_snapshot_in_branch.snapshot.name
+      end
+
+      ret
     end
   end
 end
