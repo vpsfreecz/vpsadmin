098021dce756059f4d405d13dc92d116bb82cc33
Jakub Skokan <jakub.skokan@havefun.cz>
2014-10-26 13:44:28 +0100

    Backup chain: rotate both source and destination

--- models/transaction_chains/dataset/backup.rb
+++ models/transaction_chains/dataset/backup.rb
@@ -13,6 +13,9 @@ module TransactionChains
       # execution (on vpsAdmind), because it may be different!
       # That is not very nice and should be solved better in the future.
       use_chain(Dataset::Transfer, src_dataset_in_pool, dst_dataset_in_pool)
+
+      use_chain(Dataset::Rotate, src_dataset_in_pool)
+      use_chain(Dataset::Rotate, dst_dataset_in_pool)
     end
   end
 end
