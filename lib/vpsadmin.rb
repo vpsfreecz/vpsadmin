e1dbd710b5a813eee8d296f48480da5c8a2e2754
Jakub Skokan <jakub.skokan@havefun.cz>
2014-10-27 18:12:11 +0100

    Define hooks interface

--- lib/vpsadmin.rb
+++ lib/vpsadmin.rb
@@ -21,5 +21,6 @@ end
 
 require_relative 'vpsadmin/scheduler'
 require_relative 'vpsadmin/api/crypto_provider'
+require_relative 'vpsadmin/api/hooks'
 require_rel '../models'
 require_relative 'vpsadmin/api'
