76a658d9268a5aa175961a5336b9b68884b4132c
Jakub Skokan <jakub.skokan@havefun.cz>
2014-11-01 16:21:30 +0100

    Chain backup: do not snapshot

--- models/transaction_chains/dataset/backup.rb
+++ models/transaction_chains/dataset/backup.rb
@@ -7,8 +7,6 @@ module TransactionChains
       lock(src_dataset_in_pool)
       lock(dst_dataset_in_pool)
 
-      use_chain(Dataset::Snapshot, src_dataset_in_pool)
-
       # The transfer transaction MUST retrieve the snapshot name before
       # execution (on vpsAdmind), because it may be different!
       # That is not very nice and should be solved better in the future.
