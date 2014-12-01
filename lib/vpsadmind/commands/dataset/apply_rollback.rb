ed7e325d6ac53db661e3d6b3370407875f073b1c
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-01 16:43:47 +0100

    Dataset rollback: preserve selected ZFS properties
    Before the original snapshot is destroyed, selected properties
    are remembered and set to the new snapshot.

--- lib/vpsadmind/commands/dataset/apply_rollback.rb
+++ lib/vpsadmind/commands/dataset/apply_rollback.rb
@@ -13,12 +13,22 @@ module VpsAdmind
         zfs(:rename, nil, "#{origin}/#{child} #{origin}.rollback/#{child}")
       end
 
+      # Save original properties
+      state = dataset_properties(origin, [
+          :atime, :compression, :mountpoint, :quota,
+          :recordsize, :refquota, :sync
+      ])
+
       # Destroy the original dataset
       zfs(:destroy, '-r', origin)
 
       # Mova the rollbacked one in its place
       zfs(:rename, nil, "#{origin}.rollback #{origin}")
-      zfs(:inherit, 'canmount', origin)
+
+      # Restore original properties
+      state.apply_to(origin)
+
+      zfs(:set, 'canmount=on', origin)
       zfs(:mount, nil, origin)
     end
   end
--- lib/vpsadmind/utils/zfs.rb
+++ lib/vpsadmind/utils/zfs.rb
@@ -1,6 +1,48 @@
 module VpsAdmind
   # Utilities for zfs
   module Utils::Zfs
+    class DatasetInfo
+      Property = Struct.new(:source, :value)
+
+      INHERIT_EXCEPTIONS = [:quota, :refquota, :canmount]
+
+      def initialize(cmd, str)
+        @cmd = cmd
+        @props = {}
+
+        str.split("\n").each do |line|
+          prop = line.split("\t")
+
+          @props[ prop[0].to_sym ] = Property.new(prop[1].to_sym, prop[2])
+        end
+      end
+
+      def method_missing(name)
+        if @props.has_key?(name)
+          return @props[name].value
+        end
+
+        super(name)
+      end
+
+      def apply_to(ds)
+        @props.each do |name, prop|
+          case prop.source
+            when :local, :none
+              @cmd.zfs(:set, "#{name}=\"#{prop.value}\"", ds)
+
+            when :default, :inherited
+              next if INHERIT_EXCEPTIONS.include?(name)
+
+              @cmd.zfs(:inherit, name, ds)
+            else
+              # :temporary, nothing to do
+          end
+        end
+        true
+      end
+    end
+
     # Shortcut for #syscmd
     def zfs(cmd, opts, component, valid_rcs = [])
       syscmd("#{$CFG.get(:bin, :zfs)} #{cmd.to_s} #{opts} #{component}", valid_rcs)
@@ -8,6 +50,14 @@ module VpsAdmind
 
     def list_snapshots(ds)
       zfs(:list, "-r -t snapshot -H -o name", ds)[:output].split()
+    end
+
+    def dataset_properties(ds, names)
+      DatasetInfo.new(self, zfs(
+          :get,
+          "-H -p -o property,source,value #{names.is_a?(Array) ? names.join(',') : names}",
+          ds
+      )[:output])
     end
   end
 end
