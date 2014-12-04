ac927d2a864574b8adb5eee3ccb9f9929e892f68
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-04 14:03:44 +0100

    Transaction RemoteRollback: fix tree and branch params
    Branch and tree parameters were set badly. Head was always used,
    instead of the real branch/tree where the snapshot is.

--- models/transactions/storage/remote_rollback.rb
+++ models/transactions/storage/remote_rollback.rb
@@ -6,14 +6,15 @@ module Transactions::Storage
     def params(dataset_in_pool, snapshot_in_pool)
       self.t_server = snapshot_in_pool.dataset_in_pool.pool.node_id
 
-      tree = snapshot_in_pool.dataset_in_pool.dataset_trees.find_by!(head: true)
+      snap_in_branch = snapshot_in_pool.snapshot_in_pool_in_branches
+                           .where.not(confirmed: SnapshotInPoolInBranch.confirmed(:confirm_destroy)).take!
 
       {
           primary_node_addr: dataset_in_pool.pool.node.addr,
           primary_pool_fs: dataset_in_pool.pool.filesystem,
           backup_pool_fs: snapshot_in_pool.dataset_in_pool.pool.filesystem,
-          tree: tree.full_name,
-          branch: tree.branches.find_by!(head: true).full_name,
+          tree: snap_in_branch.branch.dataset_tree.full_name,
+          branch: snap_in_branch.branch.full_name,
           dataset_name: dataset_in_pool.dataset.full_name,
           snapshot: snapshot_in_pool.snapshot.name
       }
