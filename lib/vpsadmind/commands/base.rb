c338d20c1733d07c7ddad09af099045c9bff3e57
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-11 15:56:19 +0100

    Command: class method #needs

--- lib/vpsadmind/commands/base.rb
+++ lib/vpsadmind/commands/base.rb
@@ -1,7 +1,28 @@
 module VpsAdmind::Commands
   class Base
+    # Mapping of module names.
+    MODULES = {
+        :system => :System,
+        :vz => :Vz,
+        :zfs => :Zfs,
+        :vps => :Vps
+    }
+
     def self.handle(type)
       VpsAdmind::Command.register(self.to_s, type)
+    end
+
+    # Includes module from VpsAdmind::Utils using mapping
+    # in Base::MODULES.
+    def self.needs(*args)
+      args.each do |arg|
+        if arg.is_a?(Array)
+          needs(arg)
+
+        else
+          send(:include, VpsAdmind::Utils.const_get(MODULES[arg]))
+        end
+      end
     end
 
     attr_accessor :output
