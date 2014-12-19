592127342750e8921e16947d0d4beb465667c095
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 10:41:59 +0100

    Dataset::Snapshot: rollback

--- lib/vpsadmind/commands/dataset/snapshot.rb
+++ lib/vpsadmind/commands/dataset/snapshot.rb
@@ -1,10 +1,16 @@
 module VpsAdmind
   class Commands::Dataset::Snapshot < Commands::Base
     handle 5204
+    needs :system, :zfs
 
     def exec
       @name = Dataset.new.snapshot(@pool_fs, @dataset_name)
       ok
+    end
+
+    def rollback
+      s = @name || get_confirmed_snapshot_name(Db.new, @snapshot_id)
+      zfs(:destroy, nil, "#{@pool_fs}/#{@dataset_name}@#{s}", [1])
     end
 
     def post_save(db)
