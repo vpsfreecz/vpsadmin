6c3305d7c6d1c2276db0803b6feac0f11e3d2a9b
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 10:43:59 +0100

    Vps::Passwd: dummy rollback

--- lib/vpsadmind/commands/vps/passwd.rb
+++ lib/vpsadmind/commands/vps/passwd.rb
@@ -6,6 +6,10 @@ module VpsAdmind
       Vps.new(@vps_id).passwd(@user, @password)
     end
 
+    def rollback
+      ok
+    end
+
     def post_save(db)
       db.prepared("UPDATE transactions SET t_param = '{}' WHERE t_id = ?", @command.id)
     end
