5687304f4f1b94b08492213b538bb2d9765cc7ae
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 18:02:37 +0100

    Vps::DnsResolver: rollback

--- lib/vpsadmind/commands/vps/dns_resolver.rb
+++ lib/vpsadmind/commands/vps/dns_resolver.rb
@@ -5,5 +5,9 @@ module VpsAdmind
     def exec
       Vps.new(@vps_id).set_params({:nameserver => @nameserver})
     end
+
+    def rollback
+      Vps.new(@vps_id).set_params({:nameserver => @original})
+    end
   end
 end
