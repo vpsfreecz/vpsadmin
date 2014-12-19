e930667c6cef47d1b2a97cad88f03aa094dfd868
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 10:44:25 +0100

    Vps::Start: rollback

--- lib/vpsadmind/commands/vps/start.rb
+++ lib/vpsadmind/commands/vps/start.rb
@@ -4,7 +4,13 @@ module VpsAdmind
 
     def exec
       @vps = Vps.new(@vps_id)
+      fail 'fuckyou'
       @vps.start
+    end
+
+    def rollback
+      @vps = Vps.new(@vps_id)
+      @vps.stop
     end
 
     def post_save(db)
