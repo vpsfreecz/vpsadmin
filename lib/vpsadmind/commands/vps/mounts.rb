d8540e72b801354537e79c4b5e181627297e7083
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 17:46:19 +0100

    Vps::Mounts: rollback
    Original action scripts are backed up and restored if necessary.

--- lib/vpsadmind/commands/vps/mounts.rb
+++ lib/vpsadmind/commands/vps/mounts.rb
@@ -4,12 +4,32 @@ module VpsAdmind
     needs :system, :vps, :zfs
 
     def exec
+      syscmd("#{$CFG.get(:bin, :cp)} -p \"#{original_path(:mount)}\" \"#{backup_path(:mount)}\"")
+      syscmd("#{$CFG.get(:bin, :cp)} -p \"#{original_path(:umount)}\" \"#{backup_path(:umount)}\"")
+
       action_script('mount')
       action_script('umount')
     end
 
     def rollback
+      if File.exists?(backup_path(:mount))
+        syscmd("#{$CFG.get(:bin, :cp)} -p \"#{backup_path(:mount)}\" \"#{original_path(:mount)}\"")
+      end
+
+      if File.exists?(backup_path(:umount))
+        syscmd("#{$CFG.get(:bin, :cp)} -p \"#{backup_path(:umount)}\" \"#{original_path(:umount)}\"")
+      end
+
       ok
+    end
+
+    protected
+    def original_path(type)
+      "#{$CFG.get(:vz, :vz_conf)}/conf/#{@vps_id}.#{type}"
+    end
+
+    def backup_path(type)
+      "#{original_path(type)}.backup"
     end
   end
 end
