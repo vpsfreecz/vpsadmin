ebe831ee50536702d93ac033ee332c043e77bd46
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 13:46:49 +0100

    Branch::Create: rollback

--- lib/vpsadmind/commands/branch/create.rb
+++ lib/vpsadmind/commands/branch/create.rb
@@ -6,17 +6,30 @@ module VpsAdmind
     include Utils::Zfs
 
     def exec
-      new_branch = "#{@pool_fs}/#{@dataset_name}/#{@tree}/#{@new_branch_name}"
-
       if @from_branch_name
-        from = "#{@pool_fs}/#{@dataset_name}/#{@tree}/#{@from_branch_name}@#{@from_snapshot}"
-
-        zfs(:clone, nil, "#{from} #{new_branch}")
+        zfs(:clone, nil, "#{from_branch}@#{@from_snapshot} #{new_branch}")
         zfs(:promote, nil, new_branch)
 
       else
         zfs(:create, nil, "#{new_branch}")
       end
+    end
+
+    def rollback
+      if @from_branch_name
+        zfs(:promote, nil, from_branch)
+      end
+
+      zfs(:destroy, nil, new_branch)
+    end
+
+    protected
+    def from_branch
+      "#{@pool_fs}/#{@dataset_name}/#{@tree}/#{@from_branch_name}"
+    end
+
+    def new_branch
+      "#{@pool_fs}/#{@dataset_name}/#{@tree}/#{@new_branch_name}"
     end
   end
 end
