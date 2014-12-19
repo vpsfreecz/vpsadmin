2dae5f2e4ea14eb90bda1451dd8530a6c3f9e2c5
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 13:58:44 +0100

    Vps::IpAdd: rollback

--- lib/vpsadmind/commands/vps/ip_add.rb
+++ lib/vpsadmind/commands/vps/ip_add.rb
@@ -5,5 +5,9 @@ module VpsAdmind
     def exec
       Vps.new(@vps_id).ip_add(@addr, @version, @shaper)
     end
+
+    def rollback
+      Vps.new(@vps_id).ip_del(@addr, @version, @shaper)
+    end
   end
 end
