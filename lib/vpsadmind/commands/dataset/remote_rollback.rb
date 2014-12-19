996218a549e490051997c1c6316434e5ccdb4409
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 13:48:16 +0100

    Dataset::RemoteRollback: rollback

--- lib/vpsadmind/commands/dataset/remote_rollback.rb
+++ lib/vpsadmind/commands/dataset/remote_rollback.rb
@@ -5,10 +5,19 @@ module VpsAdmind
     include Utils::System
 
     def exec
-      recv = "zfs recv -F #{@primary_pool_fs}/#{@dataset_name}.rollback"
+      recv = "zfs recv -F #{primary_ds}"
       send = "zfs send #{@backup_pool_fs}/#{@dataset_name}/#{@tree}/#{@branch}@#{@snapshot}"
 
       syscmd("#{send} | ssh #{@primary_node_addr} #{recv}")
+    end
+
+    def rollback
+      zfs(:destroy, nil, primary_ds)
+    end
+
+    protected
+    def primary_ds
+      "#{@primary_pool_fs}/#{@dataset_name}.rollback"
     end
   end
 end
