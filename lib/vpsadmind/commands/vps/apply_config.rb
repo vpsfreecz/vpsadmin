4ba29dc4bfb00bce625da1eeb72703a23180754d
Jakub Skokan <jakub.skokan@havefun.cz>
2014-12-19 17:39:22 +0100

    Vps::ApplyConfig: rollback
    Original config is backed up and restored if necessary. This solution
    is not that perfect, because CT may have some changes in configuration
    which will be reverted only after the CT is restarted.

--- lib/vpsadmind/commands/vps/apply_config.rb
+++ lib/vpsadmind/commands/vps/apply_config.rb
@@ -1,10 +1,54 @@
 module VpsAdmind
   class Commands::Vps::ApplyConfig < Commands::Base
     handle 2008
+    needs :system, :vz
 
     def exec
-      Vps.new(@vps_id).applyconfig(@configs)
+      n = Node.new
+
+      syscmd("#{$CFG.get(:bin, :cp)} -p \"#{cfg_path}\" \"#{cfg_backup_path}\"")
+
+      @configs.each do |cfg|
+        vzctl(:set, @vps_id, {:applyconfig => cfg, :setmode => 'restart'}, true)
+
+        path = n.conf_path("original-#{cfg}")
+
+        if File.exists?(path)
+          content = File.new(path).read
+
+          m = nil
+          quota = nil
+
+          if (m = content.match(/^DISKSPACE\=\"\d+\:(\d+)\"/))
+            quota = m[1].to_i * 1024 # vzctl saves diskspace in kB
+
+          elsif (m = content.match(/^DISKSPACE\=\"\d+[GMK]\:(\d+[GMK])\"/))
+            quota = m[1]
+          end
+
+          if quota
+            zfs(:set, "refquota=#{quota}", ve_private_ds)
+          end
+        end
+      end
       ok
+    end
+
+    def rollback
+      if File.exists?(cfg_backup_path)
+        syscmd("#{$CFG.get(:bin, :cp)} -p \"#{cfg_backup_path}\" \"#{cfg_path}\"")
+      end
+
+      ok
+    end
+
+    protected
+    def cfg_path
+      "#{$CFG.get(:vz, :vz_conf)}/conf/#{@vps_id}.conf"
+    end
+
+    def cfg_backup_path
+      "#{cfg_path}.backup"
     end
   end
 end
