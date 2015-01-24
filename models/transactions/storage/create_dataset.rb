1c08c8df210951ee4566c20c620fef0442a0f28f
Jakub Skokan <jakub.skokan@havefun.cz>
2015-01-24 17:47:57 +0100

    Storage::CreateDataset: private directory

--- models/transactions/storage/create_dataset.rb
+++ models/transactions/storage/create_dataset.rb
@@ -9,7 +9,8 @@ module Transactions::Storage
       {
           pool_fs: dataset_in_pool.pool.filesystem,
           name: dataset_in_pool.dataset.full_name,
-          options: opts
+          options: opts,
+          create_private: %w(hypervisor primary).include?(dataset_in_pool.pool.role)
       }
     end
   end
