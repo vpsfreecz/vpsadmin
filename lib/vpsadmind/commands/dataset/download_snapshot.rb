1cf9d9321dd0a284d46205393516f5100ac55197
Jakub Skokan <jakub.skokan@havefun.cz>
2015-01-26 10:46:43 +0100

    Transaction Dataset::DownloadSnapshot

---
+++ lib/vpsadmind/commands/dataset/download_snapshot.rb
@@ -0,0 +1,34 @@
+module VpsAdmind
+  class Commands::Dataset::DownloadSnapshot < Commands::Base
+    handle 5004
+    needs :system, :pool
+
+    def exec
+      # On ZoL, snapshots in .zfs are mounted using automounts, so for tar
+      # to work properly, it must be accessed before, so that it is already mounted
+      # when tar is launched.
+      Dir.entries("/#{@pool_fs}/#{@dataset_name}/.zfs/snapshot/#{@snapshot}")
+
+      syscmd("#{$CFG.get(:bin, :mkdir)} \"#{secret_dir_path}\"")
+      syscmd("#{$CFG.get(:bin, :tar)} -czf \"#{file_path}\" -C \"/#{@pool_fs}/#{@dataset_name}/.zfs/snapshot\" \"#{@snapshot}\"")
+    end
+
+    def rollback
+      syscmd("#{$CFG.get(:bin, :rm)} -f \"#{file_path}\"") if File.exists?(file_path)
+      syscmd("#{$CFG.get(:bin, :rmdir)} \"#{secret_dir_path}\"") if File.exists?(secret_dir_path)
+    end
+
+    protected
+    def snapshot_path
+
+    end
+
+    def secret_dir_path
+      "/#{@pool_fs}/#{path_to_pool_working_dir(:download)}/#{@secret_key}"
+    end
+
+    def file_path
+      "#{secret_dir_path}/#{@file_name}"
+    end
+  end
+end
