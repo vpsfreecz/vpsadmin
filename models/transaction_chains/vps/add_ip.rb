f905658a200de5f5752987a037109eb5f7f9ad83
Jakub Skokan <jakub.skokan@havefun.cz>
2015-02-05 13:18:07 +0100

    Vps IP add/del allocates and frees resources

--- models/transaction_chains/vps/add_ip.rb
+++ models/transaction_chains/vps/add_ip.rb
@@ -3,12 +3,18 @@ module TransactionChains
     label 'Add IP address'
 
     def link_chain(vps, ips)
+      use = vps.reallocate_resource!(:ipv4, vps.ipv4 + ips.size, user: vps.user)
+
       ips.each do |ip|
         lock(ip)
 
         append(Transactions::Vps::IpAdd, args: [vps, ip]) do
           edit(ip, vps_id: vps.veid)
         end
+      end
+
+      append(Transactions::Utils::NoOp, args: vps.vps_server) do
+        edit(use, value: use.value)
       end
     end
   end
--- models/transaction_chains/vps/del_ip.rb
+++ models/transaction_chains/vps/del_ip.rb
@@ -3,12 +3,18 @@ module TransactionChains
     label 'Delete IP address'
 
     def link_chain(vps, ips)
+      use = vps.reallocate_resource!(:ipv4, vps.ipv4 - ips.size, user: vps.user)
+
       ips.each do |ip|
         lock(ip)
 
         append(Transactions::Vps::IpDel, args: [vps, ip]) do
           edit(ip, vps_id: nil)
         end
+      end
+
+      append(Transactions::Utils::NoOp, args: vps.vps_server) do
+        edit(use, value: use.value)
       end
     end
   end
