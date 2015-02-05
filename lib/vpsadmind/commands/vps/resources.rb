c8a32c7445c2ae748e7c43d423b87967d72e0090
Jakub Skokan <jakub.skokan@havefun.cz>
2015-02-05 13:13:55 +0100

    Transaction Vps::Resources

---
+++ lib/vpsadmind/commands/vps/resources.rb
@@ -0,0 +1,36 @@
+module VpsAdmind
+  class Commands::Vps::Resources < Commands::Base
+    handle 2003
+    needs :system, :vz
+
+    def exec
+      @resources.each do |r|
+        vzctl(:set, @vps_id, translate_value(r['resource'], r['value']), true)
+      end
+
+      ok
+    end
+
+    def rollback
+      @resources.each do |r|
+        vzctl(:set, @vps_id, translate_value(r['resource'], r['original']), true)
+      end
+
+      ok
+    end
+
+    protected
+    def translate_value(name, value)
+      case name
+        when 'cpu'
+          {cpus: value, cpulimit: value*100}
+
+        when 'memory'
+          {ram: "#{value}M"}
+
+        when 'swap'
+          {swap: "#{value}M"}
+      end
+    end
+  end
+end
