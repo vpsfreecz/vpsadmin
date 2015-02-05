c0d9f20620ef94113bdfbba02c8f79c6c7a49a3e
Jakub Skokan <jakub.skokan@havefun.cz>
2015-02-05 16:06:46 +0100

    User::Create: create environment configs

--- models/transaction_chains/user/create.rb
+++ models/transaction_chains/user/create.rb
@@ -7,6 +7,19 @@ module TransactionChains
 
     def link_chain(user)
       user.save!
+
+      # Create environment configs
+      ::Environment.all.each do |env|
+        ::EnvironmentUserConfig.create!(
+            environment: env,
+            user: user,
+            can_create_vps: env.can_create_vps,
+            can_destroy_vps: env.can_destroy_vps,
+            vps_lifetime: env.vps_lifetime,
+            max_vps_count: env.max_vps_count
+        )
+      end
+
       user.call_class_hooks_for(:create, self, args: [user])
     end
   end
