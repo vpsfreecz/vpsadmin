f3f25985b0ed3047d27ca4e7006f4b643f0d4bae
Jakub Skokan <jakub.skokan@havefun.cz>
2015-02-26 20:14:03 +0100

    Vps::CopyConfigs: local copy, destination veid

--- models/transactions/vps/copy_configs.rb
+++ models/transactions/vps/copy_configs.rb
@@ -3,12 +3,14 @@ module Transactions::Vps
     t_name :vps_copy_configs
     t_type 4001
 
-    def params(vps, dst_node)
+    def params(vps, dst_node, dst_vps = nil)
       self.t_vps = vps.vps_id
       self.t_server = dst_node.id
 
       {
-          src_node_addr: vps.node.addr
+          src_node_addr: vps.node.addr,
+          local: vps.vps_server == dst_node.id,
+          dst_vps: (dst_vps && dst_vps.id) || vps.vps_id
       }
     end
   end
