c0e9e91d5a5222b968c677adc5004a2968ccbb2d
Jakub Skokan <jakub.skokan@havefun.cz>
2015-02-26 20:12:32 +0100

    Vps::CopyConfigs: local copy, copy to different veid

--- lib/vpsadmind/commands/vps/copy_configs.rb
+++ lib/vpsadmind/commands/vps/copy_configs.rb
@@ -1,18 +1,39 @@
 module VpsAdmind
   class Commands::Vps::CopyConfigs < Commands::Base
     handle 4001
+    needs :system, :vz, :vps
 
-    include Utils::System
-    include Utils::Vz
+    CONFIGS = [:mount, :umount, :conf]
 
     def exec
-      scp("#{@src_node_addr}:#{$CFG.get(:vz, :vz_conf)}/conf/#{@vps_id}.*", "#{$CFG.get(:vz, :vz_conf)}/conf/")
+      if @local
+        CONFIGS.each do |suffix|
+          cfg = "#{$CFG.get(:vz, :vz_conf)}/conf/#{@vps_id}.#{suffix}"
 
-      vzctl(:set, @vps_id, {:onboot => 'no'}, true)
+          if File.exists?(cfg)
+            syscmd("#{$CFG.get(:bin, :cp)} -p #{cfg} #{$CFG.get(:vz, :vz_conf)}/conf/#{@dst_vps}.#{suffix}")
+          end
+        end
+
+        if @vps_id != @dst_vps
+            vzctl(:set, @dst_vps, {:root => ve_root(@dst_vps), :private => ve_private(@dst_vps)})
+        end
+
+      else
+        CONFIGS.each do |suffix|
+          # Accept return code 1 - file not found
+          scp(
+              "#{@src_node_addr}:#{$CFG.get(:vz, :vz_conf)}/conf/#{@vps_id}.#{suffix}", "#{$CFG.get(:vz, :vz_conf)}/conf/#{@dst_vps}.#{suffix}",
+              [suffix != :conf ? 1 : 0]
+          )
+        end
+      end
+
+      vzctl(:set, @dst_vps, {:onboot => 'no'}, true)
     end
 
     def rollback
-      Dir.glob("#{$CFG.get(:vz, :vz_conf)}/conf/#{@vps_id}.{mount,umount,conf}").each do |cfg|
+      Dir.glob("#{$CFG.get(:vz, :vz_conf)}/conf/#{@dst_vps}.{#{CONFIGS.join(',')}").each do |cfg|
         syscmd("#{$CFG.get(:bin, :mv)} #{cfg} #{cfg}.destroyed")
       end
 
