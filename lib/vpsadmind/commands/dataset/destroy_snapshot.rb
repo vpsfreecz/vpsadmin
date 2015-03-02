014bca15ed4a88c832bd9172b0a6f5e1bea27853
Jakub Skokan <jakub.skokan@havefun.cz>
2015-03-02 13:04:36 +0100

    Dataset::DestroySnapshot: confirm snapshot name

--- lib/vpsadmind/commands/dataset/destroy_snapshot.rb
+++ lib/vpsadmind/commands/dataset/destroy_snapshot.rb
@@ -1,16 +1,22 @@
 module VpsAdmind
   class Commands::Dataset::DestroySnapshot < Commands::Base
     handle 5212
-
-    include Utils::System
-    include Utils::Zfs
+    needs :system, :zfs
 
     def exec
+      if @snapshot['confirmed'] == 1
+        snapshot = @snapshot['name']
+      else
+        db = Db.new
+        snapshot = get_confirmed_snapshot_name(db, @snapshot['id'])
+        db.close
+      end
+
       if @branch
-        zfs(:destroy, nil, "#{@pool_fs}/#{@dataset_name}/#{@tree}/#{@branch}@#{@snapshot}")
+        zfs(:destroy, nil, "#{@pool_fs}/#{@dataset_name}/#{@tree}/#{@branch}@#{snapshot}")
 
       else
-        zfs(:destroy, nil, "#{@pool_fs}/#{@dataset_name}@#{@snapshot}")
+        zfs(:destroy, nil, "#{@pool_fs}/#{@dataset_name}@#{snapshot}")
       end
     end
   end
