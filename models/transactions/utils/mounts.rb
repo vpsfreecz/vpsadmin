d7e07d8f8861bc679fdd58eea8a5b5ecc37bfe08
Jakub Skokan <jakub.skokan@havefun.cz>
2015-03-04 20:35:35 +0100

    Mount transaction parameters: fix snapshot id

--- models/transactions/utils/mounts.rb
+++ models/transactions/utils/mounts.rb
@@ -43,7 +43,7 @@ module Transactions::Utils
           m.update({
               pool_fs: mnt.snapshot_in_pool.dataset_in_pool.pool.filesystem,
               dataset_name: mnt.snapshot_in_pool.dataset_in_pool.dataset.full_name,
-              snapshot_id: mnt.snapshot_in_pool_id,
+              snapshot_id: mnt.snapshot_in_pool.snapshot_id,
               snapshot: mnt.snapshot_in_pool.snapshot.name,
               dst: mnt.dst,
               mount_opts: mnt.mount_opts,
