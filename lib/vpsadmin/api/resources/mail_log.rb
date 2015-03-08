253d8d6ace6b4a23c17368173f8db5975e9f6306
Jakub Skokan <jakub.skokan@havefun.cz>
2015-03-08 14:47:14 +0100

    Resource MailLog

---
+++ lib/vpsadmin/api/resources/mail_log.rb
@@ -0,0 +1,70 @@
+module VpsAdmin::API::Resources
+  class MailLog < HaveAPI::Resource
+    version 1
+    model ::MailLog
+    desc 'Browse sent mails'
+
+    params(:all) do
+      resource User, value_label: :login
+      string :to
+      string :cc
+      string :bcc
+      string :from
+      string :reply_to
+      string :return_path
+      string :message_id
+      string :in_reply_to
+      string :references
+      string :subject
+      string :text_plain
+      string :text_html
+      resource MailTemplate
+      #resource TransactionChain::Transaction
+      datetime :created_at
+    end
+
+    class Index < HaveAPI::Actions::Default::Index
+      desc 'List mails'
+
+      output(:object_list) do
+        use :all
+      end
+
+      authorize do |u|
+        allow if u.role == :admin
+      end
+
+      def query
+        ::MailLog.all
+      end
+
+      def count
+        query.count
+      end
+
+      def exec
+        with_includes(query).offset(input[:offset]).limit(input[:limit])
+      end
+    end
+
+    class Show < HaveAPI::Actions::Default::Show
+      desc 'View mail'
+
+      output do
+        use :all
+      end
+
+      authorize do |u|
+        allow if u.role == :admin
+      end
+
+      def prepare
+        @mail = ::MailLog.find(params[:mail_log_id])
+      end
+
+      def exec
+        @mail
+      end
+    end
+  end
+end
