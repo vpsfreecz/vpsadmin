3e39335aeb4b1431804c872c5c710e68c4735986
Jakub Skokan <jakub.skokan@havefun.cz>
2015-03-18 17:54:39 +0100

    Chain Vps::ShaperSet: add param ips

--- models/transaction_chains/vps/shaper_set.rb
+++ models/transaction_chains/vps/shaper_set.rb
@@ -2,10 +2,10 @@ module TransactionChains
   class Vps::ShaperSet < ::TransactionChain
     label 'Set shaper'
 
-    def link_chain(vps)
+    def link_chain(vps, ips = nil)
       set_concerns(:affect, [vps.class.name, vps.id])
 
-      vps.ip_addresses.all.each do |ip|
+      (ips || vps.ip_addresses.all).each do |ip|
         lock(ip)
 
         append(Transactions::Shaper::Set, args: [ip, vps])
