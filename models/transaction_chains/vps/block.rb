5bcafb79d3b6bf1bd7f4314b94c7f1a172a264c8
Jakub Skokan <jakub.skokan@havefun.cz>
2015-07-22 14:00:50 +0200

    Vps block/unblock: send notification mail

--- models/transaction_chains/vps/block.rb
+++ models/transaction_chains/vps/block.rb
@@ -4,6 +4,14 @@ module TransactionChains
 
     def link_chain(vps, target, state, log)
       use_chain(Vps::Stop, args: vps)
+
+      mail(:vps_suspend, {
+          user: vps.user,
+          vars: {
+              vps: vps,
+              state: log
+          }
+      }) if target
     end
   end
 end
--- models/transaction_chains/vps/unblock.rb
+++ models/transaction_chains/vps/unblock.rb
@@ -4,6 +4,14 @@ module TransactionChains
 
     def link_chain(vps, target, state, log)
       use_chain(Vps::Start, args: vps)
+      
+      mail(:vps_resume, {
+          user: vps.user,
+          vars: {
+              vps: vps,
+              state: log
+          }
+      }) if target
     end
   end
 end
