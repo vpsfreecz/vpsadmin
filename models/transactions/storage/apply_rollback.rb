440b07e1126145ffa9bbbbe94a5a08316de3b1f2
Jakub Skokan <jakub.skokan@havefun.cz>
2015-07-22 19:25:24 +0200

    Storage:ApplyRollback: fix params, send all descendants

--- models/transactions/storage/apply_rollback.rb
+++ models/transactions/storage/apply_rollback.rb
@@ -9,16 +9,21 @@ module Transactions::Storage
 
       children = []
 
-      Dataset.children_of(dataset_in_pool.dataset)
+      ::Dataset.descendants_of(dataset_in_pool.dataset)
         .joins(:dataset_in_pools)
-        .where(dataset_in_pools: {pool: dataset_in_pool.pool}) do |ds|
-        children << ds.name
+        .where(
+            dataset_in_pools: {pool: dataset_in_pool.pool}
+        ).order('full_name').each do |ds|
+        children << {
+            name: ds.name,
+            full_name: ds.full_name
+        }
       end
 
       {
           pool_fs: dataset_in_pool.pool.filesystem,
           dataset_name: dataset_in_pool.dataset.full_name,
-          child_datasets: children
+          descendant_datasets: children
       }
     end
   end
