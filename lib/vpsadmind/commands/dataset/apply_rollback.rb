24fedd6dd429b25933c2733a060cb32a39060864
Jakub Skokan <jakub.skokan@havefun.cz>
2015-07-22 19:26:14 +0200

    Dataset::ApplyRollback: handle subdatasets better

--- lib/vpsadmind/commands/dataset/apply_rollback.rb
+++ lib/vpsadmind/commands/dataset/apply_rollback.rb
@@ -8,9 +8,36 @@ module VpsAdmind
     def exec
       origin = "#{@pool_fs}/#{@dataset_name}"
 
+      zfs(:umount, nil, origin)
+
       # Move subdatasets from original dataset to the rollbacked one
-      @child_datasets.each do |child|
-        zfs(:rename, nil, "#{origin}/#{child} #{origin}.rollback/#{child}")
+      children = []
+      descendants = []
+
+      # Sort direct children and descendants
+      @descendant_datasets.each do |ds|
+        ds['relative_name'] = ds['full_name'].sub("#{@dataset_name}/", '')
+        puts "Relative: #{ds['full_name']} => #{ds['relative_name']}"
+
+        if /\// =~ ds['relative_name']
+          descendants << ds
+        else
+          children << ds
+        end
+      end
+      
+      # Disable mount for all descendants
+      @descendant_datasets.reverse.each do |ds|
+        zfs(:set, 'canmount=off', "#{origin}/#{ds['relative_name']}")
+      end
+
+      # Rename direct children
+      children.each do |child|
+        zfs(
+            :rename,
+            nil,
+            "#{origin}/#{child['relative_name']} #{origin}.rollback/#{child['relative_name']}"
+        )
       end
 
       # Save original properties
@@ -22,14 +49,22 @@ module VpsAdmind
       # Destroy the original dataset
       zfs(:destroy, '-r', origin)
 
-      # Mova the rollbacked one in its place
+      # Move the rollbacked one in its place
       zfs(:rename, nil, "#{origin}.rollback #{origin}")
 
       # Restore original properties
       state.apply_to(origin)
 
+      # Restore canmount and mount all datasets
       zfs(:set, 'canmount=on', origin)
       zfs(:mount, nil, origin)
+
+      @descendant_datasets.each do |ds|
+        zfs(:set, 'canmount=on', "#{origin}/#{ds['relative_name']}")
+        zfs(:mount, nil, "#{origin}/#{ds['relative_name']}")
+      end
+
+      ok
     end
   end
 end
