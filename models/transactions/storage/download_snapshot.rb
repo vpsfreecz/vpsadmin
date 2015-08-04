2be2e7857ddcf70ab341141d50eaa38b6c22c9a8
Jakub Skokan <jakub.skokan@havefun.cz>
2015-08-04 11:29:32 +0200

    Storage::DownloadSnapshot: handle branching correctly

--- models/transactions/storage/download_snapshot.rb
+++ models/transactions/storage/download_snapshot.rb
@@ -6,13 +6,25 @@ module Transactions::Storage
     def params(dl, snapshot_in_pool)
       self.t_server = dl.pool.node_id
 
-      {
+      ret = {
           pool_fs: dl.pool.filesystem,
           dataset_name: snapshot_in_pool.dataset_in_pool.dataset.full_name,
           snapshot: snapshot_in_pool.snapshot.name,
           secret_key: dl.secret_key,
           file_name: dl.file_name
       }
+
+      if snapshot_in_pool.dataset_in_pool.pool.role == 'backup'
+        in_branch = ::SnapshotInPoolInBranch.joins(branch: [:dataset_tree]).find_by!(
+            dataset_trees: {dataset_in_pool_id: snapshot_in_pool.dataset_in_pool_id},
+            snapshot_in_pool_id: snapshot_in_pool.id
+        )
+
+        ret[:tree] = in_branch.branch.dataset_tree.full_name
+        ret[:branch] = in_branch.branch.full_name
+      end
+
+      ret
     end
   end
 end
