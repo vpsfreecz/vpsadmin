2ffff171bfea9ddde9e0ed022170aec41d7249cd
Jakub Skokan <jakub.skokan@havefun.cz>
2015-08-07 10:17:32 +0200

    Storage::CloneSnapshot: handle snapshots from backup pools

--- models/transactions/storage/clone_snapshot.rb
+++ models/transactions/storage/clone_snapshot.rb
@@ -7,12 +7,24 @@ module Transactions::Storage
     def params(snapshot_in_pool)
       self.t_server = snapshot_in_pool.dataset_in_pool.pool.node_id
 
-      {
+      ret = {
           pool_fs: snapshot_in_pool.dataset_in_pool.pool.filesystem,
           dataset_name: snapshot_in_pool.dataset_in_pool.dataset.full_name,
           snapshot_id: snapshot_in_pool.snapshot_id,
           snapshot: snapshot_in_pool.snapshot.name
       }
+
+      if snapshot_in_pool.dataset_in_pool.pool.role == 'backup'
+        in_branch = ::SnapshotInPoolInBranch.joins(branch: [:dataset_tree]).find_by!(
+            dataset_trees: {dataset_in_pool_id: snapshot_in_pool.dataset_in_pool_id},
+            snapshot_in_pool_id: snapshot_in_pool.id
+        )
+
+        ret[:dataset_tree] = in_branch.branch.dataset_tree.full_name
+        ret[:branch] = in_branch.branch.full_name
+      end
+
+      ret
     end
   end
 end
