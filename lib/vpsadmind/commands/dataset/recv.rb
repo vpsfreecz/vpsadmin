a14f3f1fc2c9d73fa53ada786a7e8167a987b964
Jakub Skokan <jakub.skokan@havefun.cz>
2015-08-14 10:41:43 +0200

    Dataset::Recv: tell nc to not read from stdin

--- lib/vpsadmind/commands/dataset/recv.rb
+++ lib/vpsadmind/commands/dataset/recv.rb
@@ -6,7 +6,7 @@ module VpsAdmind
     def exec
       ds_name = @branch ? "#{@dataset_name}/#{@tree}/#{@branch}" : @dataset_name
       recv = "zfs recv -F #{@dst_pool_fs}/#{ds_name}"
-      cmd = "nc -l #{@addr} #{@port} | #{recv}"
+      cmd = "nc -d -l #{@addr} #{@port} | #{recv}"
 
       log(:work, self, "fork #{cmd}")
 
