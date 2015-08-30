beb2d73b7bf7dfe2bb70fbc3bcd82c81b671a872
Jakub Skokan <jakub.skokan@havefun.cz>
2015-08-30 10:39:51 +0200

    Commands::Status: show subtasks

--- lib/vpsadmindctl/commands/status.rb
+++ lib/vpsadmindctl/commands/status.rb
@@ -12,6 +12,10 @@ module VpsAdmindCtl::Commands
       opts.on('-c', '--consoles', 'List exported consoles') do
         @opts[:consoles] = true
       end
+      
+      opts.on('-t', '--subtasks', 'List subtasks') do
+        @opts[:subtasks] = true
+      end
 
       opts.on('-w', '--workers', 'List workers') do
         @opts[:workers] = true
@@ -53,11 +57,29 @@ module VpsAdmindCtl::Commands
         end
       end
 
-      unless @opts[:workers] || @opts[:consoles]
+      if @opts[:subtasks]
+        puts sprintf('%-10s %-10s %-20s %s', 'CHAIN', 'PID', 'STATE', 'NAME') if @opts[:header]
+
+        @res[:subprocesses].sort { |a, b| a[0].to_s.to_i <=> b[0].to_s.to_i }.each do |chain_tasks|
+          chain_tasks[1].each do |task|
+            info = process_info(task)
+            puts sprintf(
+                '%-10d %-10d %-20s %s',
+                chain_tasks[0].to_s,
+                task,
+                info[:state],
+                info[:name]
+            )
+          end
+        end
+      end
+
+      unless @opts[:workers] || @opts[:consoles] || @opts[:subtasks]
         puts "   Version: #{@vpsadmind.version}"
         puts "     State: #{state}"
         puts "    Uptime: #{format_duration(Time.new.to_i - @res[:start_time])}"
         puts "  Consoles: #{@res[:export_console] ? @res[:consoles].size : 'disabled'}"
+        puts "  Subtasks: #{@res[:subprocesses].inject(0) { |sum, v| sum + v[1].size }}"
         puts "Queue size: #{@res[:queue_size]}"
         puts "    Queues:"
 
@@ -95,6 +117,18 @@ module VpsAdmindCtl::Commands
          150 => 'restart',
          200 => 'update',
       }[s]
+    end
+
+    def process_info(pid)
+      ret = {}
+      s = File.open("/proc/#{pid}/status").read
+      
+      ret[:name] = /^Name:([^\n]+)/.match(s)[1].strip
+      ret[:state] = /^State:([^\n]+)/.match(s)[1].strip
+      ret
+
+    rescue Errno::ENOENT, NoMethodError => e
+      {}
     end
   end
 end
