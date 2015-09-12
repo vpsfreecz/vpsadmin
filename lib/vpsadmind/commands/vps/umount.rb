3b71a7267f604c56af6b93d90390070edd59e57e
Jakub Skokan <jakub.skokan@havefun.cz>
2015-09-12 11:32:12 +0200

    Vps::Umount: skip rollback if umount failed due to device busy

--- lib/vpsadmind/commands/vps/umount.rb
+++ lib/vpsadmind/commands/vps/umount.rb
@@ -14,7 +14,11 @@ module VpsAdmind
           syscmd("#{$CFG.get(:bin, :umount)} #{mnt['umount_opts']} #{dst}")
 
         rescue CommandFailed => e
-          raise e if e.rc != 1 || /^umount: #{dst}: not mounted$/ !~ e.output
+          if e.rc != 1 || /^umount: #{dst}: not mounted$/ !~ e.output
+            @skip_rollback = /^umount2: Device or resource busy$/ =~ e.output
+
+            raise e
+          end
         end
 
         runscript('postumount', mnt['postumount']) if mnt['runscripts']
@@ -24,11 +28,17 @@ module VpsAdmind
     end
 
     def rollback
-      call_cmd(Commands::Vps::Mount, {
-          :vps_id => @vps_id,
-          :mounts => @mounts.reverse,
-          :runscripts => false
-      })
+      if @skip_rollback
+        log(:debug, self, 'Skipping rollback of Vps::Umount')
+        ok
+
+      else
+        call_cmd(Commands::Vps::Mount, {
+            :vps_id => @vps_id,
+            :mounts => @mounts.reverse,
+            :runscripts => false
+        })
+      end
     end
   end
 end
