7f6ef5739bc61c2e4d8f857b40b6ffd290b665e5
Jakub Skokan <jakub.skokan@havefun.cz>
2015-12-16 14:54:58 +0100

    Status: remove VEID column

--- lib/vpsadmindctl/commands/status.rb
+++ lib/vpsadmindctl/commands/status.rb
@@ -33,16 +33,15 @@ module VpsAdmindCtl::Commands
     def process
       if @opts[:workers]
         puts sprintf(
-          '%-8s %-8s %-5s %-20.19s %-5s %-18.16s %-8s %s',
-          'QUEUE', 'TRANS', 'VEID', 'HANDLER', 'TYPE', 'TIME', 'PID', 'STEP'
+          '%-8s %-8s %-20.19s %-5s %-18.16s %-8s %s',
+          'QUEUE', 'TRANS', 'HANDLER', 'TYPE', 'TIME', 'PID', 'STEP'
         ) if @opts[:header]
 
         @res[:queues].each do |name, queue|
           queue[:workers].sort { |a, b| a[0].to_s.to_i <=> b[0].to_s.to_i }.each do |w|
-            puts sprintf('%-8s %-8d %-5d %-20.19s %-5d %-18.16s %-8s %s',
+            puts sprintf('%-8s %-8d %-20.19s %-5d %-18.16s %-8s %s',
                          name,
                          w[1][:id],
-                         w[0].to_s,
                          w[1][:handler],
                          w[1][:type],
                          w[1][:start] ? format_duration(Time.new.to_i - w[1][:start]) : '---',
