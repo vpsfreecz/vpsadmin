f988f66e6a58ad132ede52c546fc82b56d261bcd
Jakub Skokan <jakub.skokan@havefun.cz>
2015-12-29 16:17:40 +0100

    Bump version to v2.0.0

--- lib/version.lib.php
+++ lib/version.lib.php
@@ -5,7 +5,7 @@
     Copyright (C) 2008-2011 Pavel Snajdr, snajpa@snajpa.net
 */
 
-define("VERSION", '2.0.0-dev');
+define("VERSION", '2.0.0');
 define("DEMO_MODE", false);
 
 define("DB_VERSION", 15);
