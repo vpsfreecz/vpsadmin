2f537d739aa1823f17b9c24404eae13c52fcf6e9
Jakub Skokan <jakub.skokan@havefun.cz>
2016-03-09 17:40:22 +0100

    BaseDownload.find_or_create_dl: fix condition

--- lib/vpsadmin/cli/commands/base_download.rb
+++ lib/vpsadmin/cli/commands/base_download.rb
@@ -3,7 +3,7 @@ module VpsAdmin::CLI::Commands
     protected
     def find_or_create_dl(opts)
       @api.snapshot_download.index(snapshot: opts[:snapshot]).each do |r|
-        return [r, false] if opts[:from_snapshot] == r.from_snapshot
+        return [r, false] if opts[:from_snapshot] == (r.from_snapshot && r.from_snapshot_id)
       end
 
       [@api.snapshot_download.create(opts), true]
