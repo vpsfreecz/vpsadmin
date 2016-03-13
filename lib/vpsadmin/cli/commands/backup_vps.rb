4518a3a2ad5715703d0798580b49556c989513b6
Jakub Skokan <jakub.skokan@havefun.cz>
2016-03-13 10:47:17 +0100

    Backup vps: check id in user property

--- lib/vpsadmin/cli/commands/backup_vps.rb
+++ lib/vpsadmin/cli/commands/backup_vps.rb
@@ -6,8 +6,17 @@ module VpsAdmin::CLI::Commands
 
     def exec(args)
       if args.size == 1 && /^\d+$/ !~ args[0]
-        ds = dataset_chooser(vps_only: true)
-        super([ds.id, args[0]])
+        fs = args[0]
+
+        ds_id = read_dataset_id(fs)
+
+        if ds_id
+          super([ds_id, fs])
+
+        else
+          ds = dataset_chooser(vps_only: true)
+          super([ds.id, fs])
+        end
 
       elsif args.size == 2
         super([@api.vps.show(args[0].to_i).dataset_id, args[1]])
