92c6a3c1f3e424e386a9d98fa6d8f0ddf7a9373a
Jakub Skokan <jakub.skokan@havefun.cz>
2016-03-19 10:52:19 +0100

    Stream downloads migration: update chain type from Download to FullDownload

--- db/migrate/20160224195110_add_stream_downloads.rb
+++ db/migrate/20160224195110_add_stream_downloads.rb
@@ -2,5 +2,15 @@ class AddStreamDownloads < ActiveRecord::Migration
   def change
     add_column :snapshot_downloads, :format, :integer, null: false, default: 0
     add_column :snapshot_downloads, :from_snapshot_id, :integer, null: true
+
+    reversible do |dir|
+      dir.up do
+        ActiveRecord::Base.connection.execute(
+            "UPDATE `transaction_chains`
+             SET type = 'TransactionChains::Dataset::FullDownload'
+             WHERE type = 'TransactionChains::Dataset::Download'"
+        )
+      end
+    end
   end
 end
