27c3e051f3d130a847c644824f1c822e6de82aaa
Jakub Skokan <jakub.skokan@havefun.cz>
2016-08-08 18:07:11 +0200

    Status: show ETA for transactions with progress

--- lib/vpsadmindctl/commands/status.rb
+++ lib/vpsadmindctl/commands/status.rb
@@ -39,14 +39,14 @@ module VpsAdmindCtl::Commands
         if @opts[:header]
           if @global_opts[:parsable]
             puts sprintf(
-              '%-8s %-8s %-8s %-20.19s %-5s %8s  %12s %-8s %s',
-              'QUEUE', 'CHAIN', 'TRANS', 'HANDLER', 'TYPE', 'TIME', 'PROGRESS', 'PID', 'STEP'
+              '%-8s %-8s %-8s %-20.19s %-5s %8s  %12s %-18.16s %-8s %s',
+              'QUEUE', 'CHAIN', 'TRANS', 'HANDLER', 'TYPE', 'TIME', 'PROGRESS', 'ETA', 'PID', 'STEP'
             )
 
           else
             puts sprintf(
-              '%-8s %-8s %-8s %-20.19s %-5s %-18.16s %12s  %-8s %s',
-              'QUEUE', 'CHAIN', 'TRANS', 'HANDLER', 'TYPE', 'TIME', 'PROGRESS', 'PID', 'STEP'
+              '%-8s %-8s %-8s %-20.19s %-5s %-18.16s %12s %-18.16s  %-8s %s',
+              'QUEUE', 'CHAIN', 'TRANS', 'HANDLER', 'TYPE', 'TIME', 'PROGRESS', 'ETA', 'PID', 'STEP'
             )
           end
         end
@@ -55,9 +55,15 @@ module VpsAdmindCtl::Commands
 
         @res[:queues].each do |name, queue|
           queue[:workers].sort { |a, b| a[0].to_s.to_i <=> b[0].to_s.to_i }.each do |w|
+
+            if w[1][:progress] && w[1][:start]
+              rate = w[1][:progress][:current] / (t.to_i - w[1][:start])
+              eta = (w[1][:progress][:total] - w[1][:progress][:current]) / rate
+            end
+
             if @global_opts[:parsable]
               puts sprintf(
-                  '%-8s %-8d %-8d %-20.19s %-5d %8d %12s %-8s %s',
+                  '%-8s %-8d %-8d %-20.19s %-5d %8d %12s %-20.19s %-8s %s',
                   name,
                   w[0].to_s,
                   w[1][:id],
@@ -66,12 +72,13 @@ module VpsAdmindCtl::Commands
                   w[1][:start] ? (t.to_i - w[1][:start]).round : '-',
                   w[1][:pid] || '-',
                   w[1][:progress] ? format_progress(t, w[1][:progress]) : '-',
+                  eta ? eta : '-',
                   w[1][:step]
               )
 
             else
               puts sprintf(
-                  '%-8s %-8d %-8d %-20.19s %-5d %-18.16s %12s  %-8s  %s',
+                  '%-8s %-8d %-8d %-20.19s %-5d %-18.16s %12s %-20.19s  %-8s  %s',
                   name,
                   w[0].to_s,
                   w[1][:id],
@@ -79,6 +86,7 @@ module VpsAdmindCtl::Commands
                   w[1][:type],
                   w[1][:start] ? format_duration(t.to_i - w[1][:start]) : '-',
                   w[1][:progress] ? format_progress(t, w[1][:progress]) : '-',
+                  eta ? format_duration(eta) : '-',
                   w[1][:pid],
                   w[1][:step]
               )
