3441328cd86942b6db8b2b76a0e34c40e25e2a10
Jakub Skokan <jakub.skokan@havefun.cz>
2016-09-02 16:29:03 +0200

    IP ranges migration: set default network type to Network

--- db/migrate/20160629150716_add_ip_ranges.rb
+++ db/migrate/20160629150716_add_ip_ranges.rb
@@ -1,6 +1,6 @@
 class AddIpRanges < ActiveRecord::Migration
   def change
-    add_column :networks, :type, :string, limit: 255, null: false
+    add_column :networks, :type, :string, limit: 255, null: false, default: 'Network'
     add_column :networks, :ancestry, :string, null: true, limit: 255
     add_column :networks, :ancestry_depth, :integer, null: false, default: 0
     add_column :networks, :split_access, :integer, null: false, default: 0
