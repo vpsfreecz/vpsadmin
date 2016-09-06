b118fc7463dcd3910761f8e8a2962402f8cc9365
Jakub Skokan <jakub.skokan@havefun.cz>
2016-09-06 11:15:41 +0200

    Convert datetimes from IP traffics to UTC

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904191844) do
+ActiveRecord::Schema.define(version: 20160906090554) do
 
   create_table "api_tokens", force: true do |t|
     t.integer  "user_id",                            null: false
