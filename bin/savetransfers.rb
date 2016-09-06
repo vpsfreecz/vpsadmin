381bf6c1db95d3513eed1e292b9a3780ed651a2a
Jakub Skokan <jakub.skokan@havefun.cz>
2016-09-06 10:57:30 +0200

    savetransfers: store monthly summaries

--- bin/savetransfers.rb
+++ bin/savetransfers.rb
@@ -59,5 +59,31 @@ db.transaction do |t|
                                   bytes_in = bytes_in + values(bytes_in),
                                   bytes_out = bytes_out + values(bytes_out)
           ")
+  t.query("
+          INSERT INTO ip_traffic_monthly_summaries (
+            ip_address_id, user_id, protocol, role,
+            packets_in, packets_out, bytes_in, bytes_out,
+            created_at, year, month
+          )
+
+          SELECT
+            ip_address_id, user_id, protocol, role,
+            SUM(packets_in) AS spi, SUM(packets_out) AS spo,
+            SUM(bytes_in) AS sbi, SUM(bytes_out) AS sbo,
+            DATE_FORMAT(created_at, '%Y-%m-01 00:00:00'),
+            YEAR(created_at), MONTH(created_at)
+          FROM `ip_recent_traffics` r
+          WHERE created_at < DATE_SUB(NOW(), INTERVAL 60 SECOND)
+          GROUP BY ip_address_id,
+                   user_id,
+                   protocol,
+                   role,
+                   DATE_FORMAT(created_at, '%Y-%m-%d %H:00:00')
+
+          ON DUPLICATE KEY UPDATE packets_in = packets_in + values(packets_in),
+                                  packets_out = packets_out + values(packets_out),
+                                  bytes_in = bytes_in + values(bytes_in),
+                                  bytes_out = bytes_out + values(bytes_out)
+          ")
   t.query('DELETE FROM ip_recent_traffics WHERE created_at < DATE_SUB(NOW(), INTERVAL 60 SECOND)')
 end
