0f05dcea5d13ba8d8bacf041d8639a4a3bac3f52
Jakub Skokan <jakub.skokan@havefun.cz>
2016-09-07 15:10:08 +0200

    IP traffic: rename protocol all to other
    'all' was misleading, because it always meant all except tcp and udp,
    where as when one says 'all', it should include traffic from all
    protocols.

--- models/ip_traffic.rb
+++ models/ip_traffic.rb
@@ -2,6 +2,6 @@ class IpTraffic < ActiveRecord::Base
   belongs_to :ip_address
   belongs_to :user
 
-  enum protocol: %i(proto_all proto_tcp proto_udp)
+  enum protocol: %i(proto_other proto_tcp proto_udp)
   enum role: %i(role_public role_private)
 end
--- models/ip_traffic_monthly_summary.rb
+++ models/ip_traffic_monthly_summary.rb
@@ -2,7 +2,7 @@ class IpTrafficMonthlySummary < ActiveRecord::Base
   belongs_to :ip_address
   belongs_to :user
 
-  enum protocol: %i(proto_all proto_tcp proto_udp)
+  enum protocol: %i(proto_other proto_tcp proto_udp)
   enum role: %i(role_public role_private)
 
   def api_role
