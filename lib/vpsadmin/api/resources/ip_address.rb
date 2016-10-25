1a870a96bdfa1304281fc14d22a23d885ce2bada
Jakub Skokan <jakub.skokan@havefun.cz>
2016-10-25 08:30:46 +0200

    IpAddress.Index: users can see owned, assigned and free addresses

--- lib/vpsadmin/api/resources/ip_address.rb
+++ lib/vpsadmin/api/resources/ip_address.rb
@@ -114,9 +114,11 @@ class VpsAdmin::API::Resources::IpAddress < HaveAPI::Resource
       end
 
       if current_user.role != :admin
-        ips = ips.where(
-            'vps_ip.user_id = ? OR (vps_ip.user_id IS NULL AND vps_ip.vps_id IS NULL)',
-            current_user.id
+        ips = ips.joins('LEFT JOIN vps ON vps.vps_id = vps_ip.vps_id').where(
+            'vps_ip.user_id = ?
+             OR (vps_ip.vps_id IS NOT NULL AND vps.m_id = ?)
+             OR (vps_ip.user_id IS NULL AND vps_ip.vps_id IS NULL)',
+            current_user.id, current_user.id
         ).order('vps_ip.user_id DESC, ip_id ASC')
       end
 
