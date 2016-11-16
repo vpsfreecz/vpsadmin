25edd44215ce94cf69e1bd22d0e3478cb299843b
Jakub Skokan <jakub.skokan@havefun.cz>
2016-11-16 11:00:22 +0100

    Vps::DeployPublicKey: locking and concerns

--- models/transaction_chains/vps/deploy_public_key.rb
+++ models/transaction_chains/vps/deploy_public_key.rb
@@ -3,6 +3,9 @@ module TransactionChains::Vps
     label 'Pubkey'
 
     def link_chain(vps, key)
+      lock(vps)
+      concerns(:affect, [vps.class.name, vps.id])
+
       append_t(Transactions::Vps::DeployPublicKey, args: [vps, key]) do |t|
         t.just_create(vps.log(:deploy_public_key, {
             id: key.id,
