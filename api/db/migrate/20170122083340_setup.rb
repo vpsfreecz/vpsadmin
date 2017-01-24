211ba001d5867a92f524ea336ecc66542cbe64de
Jakub Skokan <jakub.skokan@havefun.cz>
2017-01-24 10:13:15 +0100

    Transfer data from members_changes during the migration

--- api/db/migrate/20170122083340_setup.rb
+++ api/db/migrate/20170122083340_setup.rb
@@ -12,18 +12,18 @@ class Setup < ActiveRecord::Migration
       t.timestamps
 
       # Registration fields
-      t.string      :login
-      t.string      :full_name
-      t.string      :org_name
-      t.string      :org_id
-      t.string      :email
-      t.text        :address
+      t.string      :login,               limit: 75
+      t.string      :full_name,           limit: 255
+      t.string      :org_name,            limit: 255
+      t.string      :org_id,              limit: 30
+      t.string      :email,               limit: 255
+      t.text        :address,             limit: 500
       t.integer     :year_of_birth
-      t.string      :how
-      t.string      :note
+      t.string      :how,                 limit: 500
+      t.string      :note,                limit: 500
       t.references  :os_template
       t.references  :location
-      t.string      :currency
+      t.string      :currency,            limit: 10
       t.references  :language
 
       # Change request fields
@@ -37,5 +37,82 @@ class Setup < ActiveRecord::Migration
     add_index :user_requests, :type
     add_index :user_requests, :state
     add_index :user_requests, :admin_id
+
+    reversible do |dir|
+      dir.up do
+        next unless table_exists?(:members_changes)
+
+        ActiveRecord::Base.connection.execute(
+            "INSERT INTO user_requests (
+              user_id,
+              type,
+              state,
+              ip_addr,
+              ip_addr_ptr,
+              last_mail_id,
+              admin_id,
+              admin_response,
+              created_at,
+              updated_at,
+
+              login,
+              full_name,
+              email,
+              address,
+              year_of_birth,
+              how,
+              note,
+              os_template_id,
+              location_id,
+              currency,
+              language_id,
+
+              change_reason
+            )
+
+            SELECT
+              m_applicant,
+              CASE m_type
+              WHEN 'add' THEN 'RegistrationRequest'
+              WHEN 'change' THEN 'ChangeRequest'
+              END,
+              CASE m_state
+              WHEN 'awaiting' THEN 0
+              WHEN 'approved' THEN 1
+              WHEN 'denied' THEN 2
+              WHEN 'invalid' THEN 2
+              WHEN 'ignored' THEN 3
+              END,
+              m_addr,
+              m_addr_reverse,
+              m_last_mail_id,
+              m_changed_by,
+              m_admin_response,
+              FROM_UNIXTIME(m_created),
+              FROM_UNIXTIME(m_changed_at),
+
+              m_nick,
+              m_name,
+              m_mail,
+              m_address,
+              m_year,
+              m_how,
+              m_note,
+              m_distribution,
+              m_location,
+              m_currency,
+              IFNULL((
+                SELECT id
+                FROM languages
+                WHERE code COLLATE utf8_general_ci = m_language COLLATE utf8_general_ci
+              ), 1),
+
+              m_reason
+
+            FROM members_changes
+            ORDER BY m_id"
+        )
+      end
+    end
   end
 end
