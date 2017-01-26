beb79bacb120b5f992aecf72d46ed030007b5cee
Jakub Skokan <jakub.skokan@havefun.cz>
2017-01-26 18:46:18 +0100

    Add resource UserAccount

---
+++ api/resources/user_account.rb
@@ -0,0 +1,76 @@
+module VpsAdmin::API::Resources
+  class UserAccount < HaveAPI::Resource
+    desc "Manage user's payment settings"
+    model ::UserAccount
+
+    params(:editable) do
+      integer :monthly_payment
+      datetime :paid_until
+    end
+
+    params(:all) do
+      id :id, db_name: :user_id
+      use :editable
+    end
+
+    class Index < HaveAPI::Actions::Default::Index
+      desc 'List user accounts'
+
+      output(:object_list) do
+        use :all
+      end
+
+      authorize do |u|
+        allow if u.role == :admin
+      end
+
+      def query
+        ::UserAccount.all
+      end
+
+      def count
+        query.count
+      end
+
+      def exec
+        with_includes(query).limit(input[:limit]).offset(input[:offset])
+      end
+    end
+    
+    class Show < HaveAPI::Actions::Default::Show
+      desc 'Show user account'
+
+      output do
+        use :all
+      end
+
+      authorize do |u|
+        allow if u.role == :admin
+      end
+      
+      def prepare
+        @acc = ::UserAccount.find_by!(user_id: params['user_account_id'])
+      end
+
+      def exec
+        @acc
+      end
+    end
+    
+    class Update < HaveAPI::Actions::Default::Update
+      desc 'Update user account'
+
+      input do
+        use :editable
+      end
+
+      output do
+        use :all
+      end
+
+      authorize do |u|
+        allow if u.role == :admin
+      end
+    end
+  end
+end
