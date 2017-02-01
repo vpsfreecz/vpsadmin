2244ebd6f6d190f748467cfa13e27c97b31fb5ac
Jakub Skokan <jakub.skokan@havefun.cz>
2017-02-01 11:36:54 +0100

    Location.Index: whitelist parameter remote_console_server

--- api/resources/override.rb
+++ api/resources/override.rb
@@ -2,7 +2,13 @@ module VpsAdmin::API::Resources
   Location::Index.auth false
   Location::Index.authorize do |u|
     allow if u && u.role == :admin
-    output whitelist: %i(id label environment)
+
+    if u
+      output whitelist: %i(id label environment remote_console_server)
+    else
+      output whitelist: %i(id label environment)
+    end
+
     allow
   end
 
