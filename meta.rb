525bbcdd7f03f3d2a072bfe38f87d00e17084f9f
Jakub Skokan <jakub.skokan@havefun.cz>
2017-02-01 14:15:03 +0100

    Declare sysconfig variables

--- meta.rb
+++ meta.rb
@@ -5,4 +5,12 @@ VpsAdmin::API::Plugin.register(:webui) do
   author 'Jakub Skokan'
   email 'jakub.skokan@vpsfree.cz'
   components :api
+
+  config do
+    SysConfig.register :webui, :base_url, String
+    SysConfig.register :webui, :document_title, String
+    SysConfig.register :webui, :noticeboard, Text
+    SysConfig.register :webui, :index_info_box_title, String
+    SysConfig.register :webui, :index_info_box_content, Text
+  end
 end
