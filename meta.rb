b416757091b9df379d02990e817937174eb12c69
Jakub Skokan <jakub.skokan@havefun.cz>
2017-02-03 21:22:25 +0100

    Use mail template registration mechanism

--- meta.rb
+++ meta.rb
@@ -13,5 +13,27 @@ VpsAdmin::API::Plugin.register(:payments) do
         min_user_level: 99
     SysConfig.register :plugin_payments, :api_token, String,
         label: 'API token', min_user_level: 99
+
+    MailTemplate.register :payment_accepted, vars: {
+        user: '::User',
+        account: '::UserAccount',
+        payment: '::UserPayment',
+    }
+
+    MailTemplate.register :payments_overview, vars: {
+        base_url: [String, "URL to the web UI"],
+        start: ::Time,
+        end: ::Time,
+        incoming: 'IncomingPayment relation',
+        queued: 'IncomingPayment relation',
+        unmatched: 'IncomingPayment relation',
+        processed: 'IncomingPayment relation',
+        ignored: 'IncomingPayment relation',
+        accepted: 'UserPayment relation',
+    }
+
+    MailTemplate.register :daily_report, vars: {
+        payments: Hash,
+    }
   end
 end
