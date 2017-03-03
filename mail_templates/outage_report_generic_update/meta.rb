eee7f711a1e3b011aff099c224e401d44660e855
Jakub Skokan <jakub.skokan@havefun.cz>
2017-03-03 19:43:27 +0100

    Mail templates: use local time, add time zone info

--- mail_templates/outage_report_generic_update/meta.rb
+++ mail_templates/outage_report_generic_update/meta.rb
@@ -5,6 +5,6 @@ template :outage_report_role_event do
   return_path  'podpora@vpsfree.cz'
 
   lang :en do
-    subject    "Re: <%= @o.planned ? 'Planned' : 'Unplanned' %> outage - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.strftime('%Y-%m-%d %H:%M') %>"
+    subject    "Re: <%= @o.planned ? 'Planned' : 'Unplanned' %> outage - <%= @o.outage_entities.map { |e| e.real_name }.join(', ') %> - <%= @o.begins_at.localtime.strftime('%Y-%m-%d %H:%M %Z') %>"
   end
 end
