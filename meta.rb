4f573a10cd4c2d07024f4f51e776e7038a92c686
Jakub Skokan <jakub.skokan@havefun.cz>
2017-03-20 09:51:48 +0100

    Version 2.7.0

--- meta.rb
+++ meta.rb
@@ -1,7 +1,7 @@
 VpsAdmin::API::Plugin.register(:webui) do
   name 'Web UI support'
   description 'Support for Web UI specific API endpoints'
-  version '0.1.0'
+  version '2.7.0'
   author 'Jakub Skokan'
   email 'jakub.skokan@vpsfree.cz'
   components :api
