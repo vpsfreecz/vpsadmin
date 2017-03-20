12769b037e7f6c94e5d92d6d469fb68305c418e4
Jakub Skokan <jakub.skokan@havefun.cz>
2017-03-20 10:00:43 +0100

    Version 2.7.0

--- meta.rb
+++ meta.rb
@@ -1,7 +1,7 @@
 VpsAdmin::API::Plugin.register(:requests) do
   name 'Requests'
   description 'User requests'
-  version '0.1.0'
+  version '2.7.0'
   author 'Jakub Skokan'
   email 'jakub.skokan@vpsfree.cz'
   components :api
