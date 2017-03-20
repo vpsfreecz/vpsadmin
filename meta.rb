3ea1f7cb0ba8811fcedcee19076a56ba1e3907fb
Jakub Skokan <jakub.skokan@havefun.cz>
2017-03-20 09:51:30 +0100

    Version 2.7.0

--- meta.rb
+++ meta.rb
@@ -1,7 +1,7 @@
 VpsAdmin::API::Plugin.register(:newslog) do
   name 'News log'
   description "Lets admins to announce news"
-  version '0.1.0'
+  version '2.7.0'
   author 'Jakub Skokan'
   email 'jakub.skokan@vpsfree.cz'
   components :api
