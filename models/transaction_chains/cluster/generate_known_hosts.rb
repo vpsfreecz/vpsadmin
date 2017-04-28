55a480ef990e03b5e8a697e03502a6b04ff60ed5
Jakub Skokan <jakub.skokan@havefun.cz>
2017-04-28 11:00:48 +0200

    Cluster::GenerateKnownHosts: fix query condition

--- models/transaction_chains/cluster/generate_known_hosts.rb
+++ models/transaction_chains/cluster/generate_known_hosts.rb
@@ -3,7 +3,7 @@ module TransactionChains
     label 'Known hosts'
 
     def link_chain
-      ::Node.where.not(role: 'mailer').each do |n|
+      ::Node.where.not(role: ::Node.roles[:mailer]).each do |n|
         append(Transactions::Node::GenerateKnownHosts, args: n)
       end
     end
