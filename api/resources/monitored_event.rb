126c83a4e6b0f05799ba82d3f63d74651d7894c9
Jakub Skokan <jakub.skokan@havefun.cz>
2017-05-04 10:06:05 +0200

    MonitoredEvent.Index: add filters

--- api/resources/monitored_event.rb
+++ api/resources/monitored_event.rb
@@ -17,6 +17,14 @@ module VpsAdmin::API::Resources
     end
 
     class Index < HaveAPI::Actions::Default::Index
+      input do
+        use :all, include: %i(monitor object_name object_id state user)
+        string :order, choices: %w(oldest latest longest shortest), default: 'latest',
+            fill: true
+
+        patch :limit, default: 25, fill: true
+      end
+
       output(:object_list) do
         use :all
       end
@@ -24,11 +32,18 @@ module VpsAdmin::API::Resources
       authorize do |u|
         allow if u.role == :admin
         restrict user_id: u.id
+        input blacklist: %i(user)
         allow
       end
 
       def query
-        ::MonitoredEvent.where(with_restricted)
+        q = ::MonitoredEvent.where(with_restricted)
+        q = q.where(monitor_name: input[:monitor]) if input[:monitor]
+        q = q.where(class_name: input[:object_name]) if input[:object_name]
+        q = q.where(row_id: input[:object_id]) if input[:object_id]
+        q = q.where(state: ::MonitoredEvent.states[input[:state]]) if input[:state]
+        q = q.where(user: input[:user]) if input[:user]
+        q
       end
 
       def count
@@ -36,7 +51,24 @@ module VpsAdmin::API::Resources
       end
 
       def exec
-        query.limit(input[:limit]).offset(input[:offset])
+        q = with_includes(query).limit(input[:limit]).offset(input[:offset])
+        t = ::MonitoredEvent.table_name
+
+        case input[:order]
+        when 'oldest'
+          q = q.order("#{t}.created_at")
+
+        when 'latest'
+          q = q.order("#{t}.created_at DESC")
+
+        when 'longest'
+          q = q.order("TIMESTAMPDIFF(SECOND, #{t}.created_at, #{t}.updated_at) DESC")
+
+        when 'shortest'
+          q = q.order("TIMESTAMPDIFF(SECOND, #{t}.created_at, #{t}.updated_at)")
+        end
+
+        q
       end
     end
 
