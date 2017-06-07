bba4f5f192aff0e1fba2daa6ed0f90cb17c9e939
Jakub Skokan <jakub.skokan@havefun.cz>
2017-06-07 17:02:09 +0200

    Version 2.8.0

--- meta.rb
+++ meta.rb
@@ -1,7 +1,7 @@
 VpsAdmin::API::Plugin.register(:outage_reports) do
   name 'Outage reports'
   description 'Adds support for outage reporting and mailing affected users'
-  version '0.1.0'
+  version '2.8.0'
   author 'Jakub Skokan'
   email 'jakub.skokan@vpsfree.cz'
   components :api
