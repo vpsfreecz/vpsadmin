60925e9a2cdae0cfc9d32f375588b992321553a0
Jakub Skokan <jakub.skokan@havefun.cz>
2017-09-20 09:58:58 +0200

    Version 2.9.0

--- meta.rb
+++ meta.rb
@@ -1,7 +1,7 @@
 VpsAdmin::API::Plugin.register(:monitoring) do
   name 'Monitoring'
   description 'Monitors resource usage and sends alerts'
-  version '0.1.0'
+  version '2.9.0'
   author 'Jakub Skokan'
   email 'jakub.skokan@vpsfree.cz'
   components :api
