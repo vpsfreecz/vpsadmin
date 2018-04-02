e4c20a1a49d7dad6902a150d91b5cd0e25bd18ca
Jakub Skokan <jakub.skokan@havefun.cz>
2018-04-02 19:51:06 +0200

    nodectld: set CPU and memory limits using osctl shortcuts

--- nodectld/lib/nodectld/commands/vps/resources.rb
+++ nodectld/lib/nodectld/commands/vps/resources.rb
@@ -17,15 +17,17 @@ module NodeCtld
     def set(key)
       mem = 0
       swap = 0
+      cpu_limits = []
 
       @resources.each do |r|
         case r['resource']
         when 'cpu'
-          # TODO: this cannot be done using cgroups afaik, i.e. limit number of
-          #       cores and _not_ assign concrete cpus
+          # We can't actually assign CPU cores without assigning them statically,
+          # so we just set CPU limit
+          cpu_limits << r[key] * 100
 
         when 'cpu_limit'
-          # TODO: cpu.cfs_period_us, cpu.cfs_quota_us
+          cpu_limits << r[key] if r[key] && r[key] > 0
 
         when 'memory'
           mem = r[key]
@@ -35,26 +37,23 @@ module NodeCtld
         end
       end
 
-      if mem > 0 && swap > 0
+      if mem > 0
         osctl(
-          %i(ct cgparams set),
-          [@vps_id, 'memory.limit_in_bytes', mem * 1024 * 1024]
-        )
-        osctl(
-          %i(ct cgparams set),
-          [@vps_id, 'memory.memsw.limit_in_bytes', (mem+swap) * 1024 * 1024]
+          %i(ct set memory),
+          [@vps_id, "#{mem}M", swap > 0 ? "#{swap}M" : nil].compact
         )
 
-      elsif mem > 0
-        osctl(
-          %i(ct cgparams unset),
-          [@vps_id, 'memory.memsw.limit_in_bytes'],
-          {}, {}, valid_rcs: [1]
-        )
-        osctl(
-          %i(ct cgparams set),
-          [@vps_id, 'memory.limit_in_bytes', mem * 1024 * 1024]
-        )
+      else
+        osctl(%i(ct unset memory), @vps_id)
+      end
+
+      cpu_limit = cpu_limits.min || 0
+
+      if cpu_limit > 0
+        osctl(%i(ct set cpu-limit), [@vps_id, cpu_limit.to_s])
+
+      else
+        osctl(%i(ct unset cpu-limit), @vps_id)
       end
     end
   end
