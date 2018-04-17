cd61e8875bee5d8a3244a73f172ed443ce5a187d
Jakub Skokan <jakub.skokan@havefun.cz>
2018-04-17 19:26:39 +0200

    nodectld: add ZfsStream as a subclass of OsCtl::Lib::Zfs::Stream

---
+++ nodectld/lib/nodectld/zfs_stream.rb
@@ -0,0 +1,46 @@
+require 'libosctl'
+
+module NodeCtld
+  class ZfsStream < OsCtl::Lib::Zfs::Stream
+    def initialize(fs, snapshot, from_snapshot = nil)
+      super(build_path(fs), snapshot, from_snapshot, compressed: true)
+    end
+
+    def send_recv(fs)
+      super(build_path(fs))
+    end
+
+    def command(cmd)
+      @cmd = cmd
+
+      block = Proc.new do |total, transfered, changed|
+        cmd.progress = {
+          total: size,
+          current: total,
+          unit: :mib,
+          time: Time.now,
+        }
+      end
+
+      progress(&block)
+
+      yield
+
+      @cmd = nil
+      @progress.delete(block)
+    end
+
+    protected
+    def build_path(fs)
+      path = [fs[:pool], fs[:dataset]]
+      path << fs[:tree] << fs[:branch] if fs[:branch]
+
+      File.join(*path)
+    end
+
+    def notify_exec(pipeline)
+      return unless @cmd
+      @cmd.step = pipeline.join(' | ')
+    end
+  end
+end
