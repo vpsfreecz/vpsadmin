ab1b8dc5dcc94150c5c0782a329d1731571493ac
Jakub Skokan <jakub.skokan@havefun.cz>
2018-05-01 19:01:25 +0200

    Vps::Destroy: fix dataset/container destroy order on vpsAdminOS

--- api/models/transaction_chains/vps/destroy.rb
+++ api/models/transaction_chains/vps/destroy.rb
@@ -28,21 +28,42 @@ module TransactionChains
       # Remove veth interface
       use_chain(Vps::RemoveVeth, args: vps) if vps.node.vpsadminos?
 
-      # Destroy VPS
-      append(Transactions::Vps::Destroy, args: vps) do
-        resources.each { |r| destroy(r) }
-        just_destroy(vps.vps_current_status) if vps.vps_current_status
-      end
+      if vps.node.openvz?
+        # Destroy VPS
+        append(Transactions::Vps::Destroy, args: vps) do
+          resources.each { |r| destroy(r) }
+          just_destroy(vps.vps_current_status) if vps.vps_current_status
+        end
 
-      # Destroy the underlying dataset
-      #
-      # On vpsAdminOS, all container's datasets are deleted by
-      # `Transactions::Vps::Destroy` as part of `osctl ct del`. That's why
-      # the datasets need to be actually destroyed only on OpenVZ nodes.
-      use_chain(
-          DatasetInPool::Destroy,
-          args: [vps.dataset_in_pool, {recursive: true, destroy: vps.node.openvz?}]
-      )
+        # Destroy underlying dataset
+        use_chain(
+            DatasetInPool::Destroy,
+            args: [vps.dataset_in_pool, {recursive: true}]
+        )
+
+      else # vpsAdminOS
+        # Destroy the underlying dataset, but only in database
+        #
+        # On vpsAdminOS, all container's datasets are deleted by
+        # `Transactions::Vps::Destroy` as part of `osctl ct del`. That's why
+        # the datasets need to be actually destroyed only on OpenVZ nodes.
+        #
+        # Because the datasets may have subdatasets that are mounted to
+        # the VPS that is being destroyed, we need to handle them before
+        # the container is deleted by osctld. It may issue mounts/umount
+        # transactions and osctl expects the container to exist when
+        # evaluating them.
+        use_chain(
+            DatasetInPool::Destroy,
+            args: [vps.dataset_in_pool, {recursive: true, destroy: false}]
+        )
+
+        # Destroy VPS
+        append(Transactions::Vps::Destroy, args: vps) do
+          resources.each { |r| destroy(r) }
+          just_destroy(vps.vps_current_status) if vps.vps_current_status
+        end
+      end
 
       # The dataset_in_pool_id must be unset after the dataset is actually
       # deleted, as it may fail.
