68fbb3571dd14b64432e6d2f92eca9f703c1a0d5
Jakub Skokan <jakub.skokan@havefun.cz>
2018-10-11 19:02:47 +0200

    api: add default.nix

---
+++ api/default.nix
@@ -0,0 +1,32 @@
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+
+in stdenv.mkDerivation rec {
+  name = "vpsadmin-api";
+
+  buildInputs = [
+    pkgs.ruby
+    pkgs.git
+    pkgs.libmysql
+    pkgs.mariadb
+  ];
+
+  shellHook = ''
+    export GEM_HOME="$PWD/.gems"
+    mkdir -p "$GEM_HOME"
+    export GEM_PATH="$GEM_HOME:$PWD/lib"
+    export PATH="$GEM_HOME/bin:$PATH"
+
+    BUNDLE="$GEM_HOME/bin/bundle"
+
+    [ ! -x "$BUNDLE" ] && ${pkgs.ruby}/bin/gem install bundler
+
+    export BUNDLE_PATH="$GEM_HOME"
+    export BUNDLE_GEMFILE="$PWD/Gemfile"
+
+    $BUNDLE install
+
+    export RUBYOPT=-rbundler/setup
+  '';
+}
