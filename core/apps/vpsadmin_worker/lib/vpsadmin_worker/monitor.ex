8f2eaf16b1047dba9c38c6742d15f2ad45abe250
Jakub Skokan <jakub.skokan@havefun.cz>
2019-01-09 21:41:00 +0100

    core: worker monitor can be reconnected
    If the vpsadmin_supervisor app crashes, vpsadmin_worker app keeps
    running. When the supervisor is restarted, the worker is still running
    and so instead expecting to always start a new one, we can use the
    original worker.

--- core/apps/vpsadmin_worker/lib/vpsadmin_worker/monitor.ex
+++ core/apps/vpsadmin_worker/lib/vpsadmin_worker/monitor.ex
@@ -2,7 +2,13 @@ defmodule VpsAdmin.Worker.Monitor do
   alias VpsAdmin.Worker.Monitor
 
   def run_command({t, cmd}, func) do
-    {:ok, _pid} = Monitor.Supervisor.run_command({t, cmd}, func)
+    case Monitor.Supervisor.run_command({t, cmd}, func) do
+      {:ok, _pid} = v ->
+        v
+
+      {:error, {:already_started, pid}} ->
+        {:ok, pid}
+    end
   end
 
   def report_result({_t, _cmd} = arg) do
