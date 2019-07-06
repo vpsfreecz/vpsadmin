76b5e3b0b0b2371646e1fd3ee0b5eba302a0c4cd
Jakub Skokan <jakub.skokan@havefun.cz>
2019-07-06 18:08:11 +0200

    webui: remove unused function sec2hms

--- webui/pages/page_backup.php
+++ webui/pages/page_backup.php
@@ -6,18 +6,6 @@
     Web-admin interface for OpenVZ (see http://openvz.org)
     Copyright (C) 2008-2011 Pavel Snajdr, snajpa@snajpa.net
 */
-function sec2hms ($sec, $padHours = false) {
-	$hms = "";
-	$hours = intval(intval($sec) / 3600);
-	$hms .= ($padHours)
-		? str_pad($hours, 2, "0", STR_PAD_LEFT). ":"
-		: $hours. ":";
-	$minutes = intval(($sec / 60) % 60);
-	$hms .= str_pad($minutes, 2, "0", STR_PAD_LEFT). ":";
-	$seconds = intval($sec % 60);
-	$hms .= str_pad($seconds, 2, "0", STR_PAD_LEFT);
-	return $hms;
-}
 
 if ($_SESSION["logged_in"]) {
 	$vps_backups = false;
