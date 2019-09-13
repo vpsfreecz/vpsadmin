fbf12f02e1afec1fa71ab98c6b9b65575def3d86
Jakub Skokan <jakub.skokan@havefun.cz>
2019-09-13 12:28:13 +0200

    api: fix chain Vps::ShaperChange to use network interfaces

--- api/models/transaction_chains/vps/shaper_change.rb
+++ api/models/transaction_chains/vps/shaper_change.rb
@@ -3,9 +3,13 @@ module TransactionChains
     label 'Shaper*'
 
     def link_chain(ip, tx, rx)
-      lock(ip.vps)
+      lock(ip.network_interface)
+      lock(ip.network_interface.vps)
       lock(ip)
-      concerns(:affect, [ip.vps.class.name, ip.vps.id])
+      concerns(:affect, [
+        ip.network_interface.vps.class.name,
+        ip.network_interface.vps.id
+      ])
 
       append(Transactions::Vps::ShaperChange, args: [ip, tx, rx]) do
         edit(ip, max_tx: tx, max_rx: rx)
