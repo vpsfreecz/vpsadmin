2b7afd598c9c2e419bc94c735950b5c76cb5b222
Jakub Skokan <jakub.skokan@havefun.cz>
2020-04-05 11:01:53 +0200

    api: when clearning netif, removed routed-via addresses first
    We delete host addresses first, so when some route is being routed via a
    host addresses that is removed, osctl will simply remove such routes,
    but vpsAdmin doesn't know about it and tries to remove it itself, which
    fails. So now we delete routed-via addresses first and the rest later.
    It still doesn't properly handle order of removed addresses, i.e. it may
    be possible to break this code as well, but it should cover more cases
    than before.

--- api/models/transaction_chains/network_interface/clear.rb
+++ api/models/transaction_chains/network_interface/clear.rb
@@ -4,15 +4,24 @@ module TransactionChains
 
     # @param vps [::NetworkInterface]
     def link_chain(netif)
-      use_chain(NetworkInterface::DelRoute, args: [
-        netif,
-        netif.ip_addresses.joins(:network).where(
-          networks: {role: [
-            ::Network.roles[:public_access],
-            ::Network.roles[:private_access],
-          ]}
-        )
-      ])
+      routed_via = []
+      routed_direct = []
+
+      netif.ip_addresses.joins(:network).where(
+        networks: {role: [
+          ::Network.roles[:public_access],
+          ::Network.roles[:private_access],
+        ]},
+      ).each do |ip|
+        if ip.routed_via_id
+          routed_via << ip
+        else
+          routed_direct << ip
+        end
+      end
+
+      use_chain(NetworkInterface::DelRoute, args: [netif, routed_via])
+      use_chain(NetworkInterface::DelRoute, args: [netif, routed_direct])
     end
   end
 end
