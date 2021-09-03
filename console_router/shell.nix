59681b685182f2cb265276388bda005b768e39ce
Jakub Skokan <jakub.skokan@havefun.cz>
2021-09-03 19:29:24 +0200

    console_router: add shell.nix

---
+++ console_router/shell.nix
@@ -0,0 +1,32 @@
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+
+in stdenv.mkDerivation rec {
+  name = "vpsadmin-console-router";
+
+  buildInputs = [
+    pkgs.ruby
+    pkgs.git
+    pkgs.mariadb
+    pkgs.mariadb-connector-c
+  ];
+
+  shellHook = ''
+    export GEM_HOME="$PWD/.gems"
+    mkdir -p "$GEM_HOME"
+    export GEM_PATH="$GEM_HOME:$PWD/lib"
+    export PATH="$GEM_HOME/bin:$PATH"
+
+    BUNDLE="$GEM_HOME/bin/bundle"
+
+    [ ! -x "$BUNDLE" ] && ${pkgs.ruby}/bin/gem install bundler
+
+    export BUNDLE_PATH="$GEM_HOME"
+    export BUNDLE_GEMFILE="$PWD/Gemfile"
+
+    $BUNDLE install
+
+    export RUBYOPT=-rbundler/setup
+  '';
+}
