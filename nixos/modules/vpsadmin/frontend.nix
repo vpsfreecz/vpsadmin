71893f094c0e97749ef1a3598846e9463b26ceec
Jakub Skokan <jakub.skokan@havefun.cz>
2021-10-06 18:56:02 +0200

    nixos: add support for download-mounter to frontend

--- nixos/modules/vpsadmin/frontend.nix
+++ nixos/modules/vpsadmin/frontend.nix
@@ -19,71 +19,111 @@ let
       '';
     };
 
-  appModule =
-    app:
-    { config, ... }:
-    {
-      options = {
-        domain = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = "Public domain";
-        };
+  baseOpts = app: config: {
+    domain = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "Public domain";
+    };
 
-        virtualHost = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = ''
-            nginx virtual host name, see <option>services.nginx.virtualHosts</option>
+    virtualHost = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = ''
+        nginx virtual host name, see <option>services.nginx.virtualHosts</option>
 
-            If not given, it defaults to the value of
-            <option>vpsadmin.frontend.${app}.domain</option>.
-          '';
-          apply = v:
-            if !isNull v then
-              v
-            else if !isNull config.domain then
-              config.domain
-            else
-              abort "unable to determine virtual host name: set domain or virtualHost";
-        };
+        If not given, it defaults to the value of
+        <option>vpsadmin.frontend.${app}.domain</option>.
+      '';
+      apply = v:
+        if !isNull v then
+          v
+        else if !isNull config.domain then
+          config.domain
+        else
+          abort "unable to determine virtual host name: set domain or virtualHost";
+    };
 
-        maintenance = {
-          enable = mkEnableOption ''
-            When enabled, the server responds with HTTP status 503 to all requests
-          '';
+    maintenance = {
+      enable = mkEnableOption ''
+        When enabled, the server responds with HTTP status 503 to all requests
+      '';
 
-          file = mkOption {
-            type = types.path;
-            default = appMaintenances.${app};
-            description = ''
-              File that is served when the maintenance is enabled
-            '';
-          };
-        };
-
-        backend = {
-          host = mkOption {
-            type = types.str;
-            description = ''
-              Hostname or IP address of the backend, usually a HAProxy instance
-            '';
-          };
-
-          port = mkOption {
-            type = types.int;
-            description = ''
-              Port number of the backend, usually a HAProxy instance
-            '';
-          };
-        };
+      file = mkOption {
+        type = types.path;
+        default = appMaintenances.${app};
+        description = ''
+          File that is served when the maintenance is enabled
+        '';
       };
     };
+
+    backend = {
+      host = mkOption {
+        type = types.str;
+        description = ''
+          Hostname or IP address of the backend, usually a HAProxy instance
+        '';
+      };
+
+      port = mkOption {
+        type = types.int;
+        description = ''
+          Port number of the backend, usually a HAProxy instance
+        '';
+      };
+    };
+  };
+
+  baseModule =
+    app:
+    { config, ... }:
+    let
+      opts = baseOpts app config;
+    in {
+      options = {
+        inherit (opts) domain virtualHost maintenance backend;
+      };
+    };
+
+  downloadMounterModule =
+    app:
+    { config, ... }:
+    let
+      opts = baseOpts app config;
+    in {
+      options = {
+        inherit (opts) domain virtualHost maintenance;
+      };
+    };
+
+  appModule = app: {
+    api = baseModule app;
+
+    console-router = baseModule app;
+
+    download-mounter = downloadMounterModule app;
+
+    webui = baseModule app;
+  }.${app};
 
   appMaintenances = {
     api = pkgs.writeText "maintenance.json" ''{"status":false,"message":"Server under maintenance."}'';
 
     console-router = pkgs.writeText "maintenance.html" ''
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <title>Maintenance</title>
+      </head>
+      <body>
+      <h1>Ongoing maintenance</h1>
+      <p>The server is under maintenance. Please try again later.</p>
+      </body>
+      </html>
+    '';
+
+    download-mounter = pkgs.writeText "maintenance.html" ''
       <!DOCTYPE html>
       <html>
       <head>
@@ -110,7 +150,7 @@ let
     '';
   };
 
-  appVirtualHosts = app: name: instance: nameValuePair instance.virtualHost {
+  baseVirtualHosts = app: name: instance: nameValuePair instance.virtualHost {
     serverName = mkIf (!isNull instance.domain) instance.domain;
 
     locations = {
@@ -137,10 +177,43 @@ let
     '';
   };
 
+  downloadMounterVirtualHosts = app: name: instance: nameValuePair instance.virtualHost {
+    locations."/" = mkIf config.vpsadmin.download-mounter.enable {
+      root = config.vpsadmin.download-mounter.mountpoint;
+    };
+  };
+
+  appVirtualHosts = app: {
+    api = baseVirtualHosts app;
+
+    console-router = baseVirtualHosts app;
+
+    download-mounter = downloadMounterVirtualHosts app;
+
+    webui = baseVirtualHosts app;
+  }.${app};
+
+  appAssertions = app: instances: {
+    api = [];
+
+    console-router = [];
+
+    download-mounter = [
+      {
+        assertion = instances != {} -> config.vpsadmin.download-mounter.enable;
+        message = "vpsadmin.frontend.download-mounter requires vpsadmin.download-mounter to be enabled";
+      }
+    ];
+
+    webui = [];
+  }.${app};
+
   appConfigs = app:
     let
       instances = cfg.${app};
     in {
+      assertions = appAssertions app instances;
+
       services.nginx.virtualHosts = mapAttrs' (appVirtualHosts app) instances;
     };
 
@@ -153,6 +226,8 @@ in {
 
       console-router = appOpt "console-router";
 
+      download-mounter = appOpt "download-mounter";
+
       webui = appOpt "webui";
     };
   };
@@ -164,6 +239,15 @@ in {
           80 443
         ];
       };
+
+      # This is to enable mount propagation from the host ns to systemd services,
+      # e.g. nginx. At least in vpsAdminOS containers, the default mount
+      # propagation mode seems to be `private`. That makes it impossible to
+      # use the download mounter, because NFS mounts do not appear within
+      # the nginx's mount namespace after it is created.
+      boot.postBootCommands = ''
+        mount --make-shared /
+      '';
 
       services.nginx = {
         enable = true;
@@ -182,6 +266,8 @@ in {
     (appConfigs "api")
 
     (appConfigs "console-router")
+
+    (appConfigs "download-mounter")
 
     (appConfigs "webui")
   ]);
