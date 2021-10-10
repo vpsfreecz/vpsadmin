63c8f15e0dda3956e39170007bdbcb0430aa09eb
Jakub Skokan <jakub.skokan@havefun.cz>
2021-10-10 15:25:34 +0200

    nixos: add vpsadmin-api-shell script

--- nixos/modules/vpsadmin/api/default.nix
+++ nixos/modules/vpsadmin/api/default.nix
@@ -34,6 +34,28 @@ let
   '';
 
   forAllPorts = fn: genList (i: fn (cfg.port + i)) cfg.servers;
+
+  apiShellRcFile = pkgs.writeText "vpsadmin-api-shell.rc" ''
+    . /etc/profile
+    export PATH=${cfg.package}/ruby-env/bin:$PATH
+  '';
+
+  apiShellScript = pkgs.writeScriptBin "vpsadmin-api-shell" ''
+    #!${pkgs.bash}/bin/bash
+    exec systemd-run \
+      --unit=vpsadmin-api-shell-$(date "+%F-%T") \
+      --description="vpsAdmin API interactive shell" \
+      --pty \
+      --wait \
+      --collect \
+      --service-type=exec \
+      --working-directory=${cfg.package}/api \
+      --setenv=RACK_ENV=production \
+      --setenv=SCHEMA=${cfg.stateDir}/cache/structure.sql \
+      --uid ${cfg.user} \
+      --gid ${cfg.group} \
+      ${pkgs.bashInteractive}/bin/bash --rcfile ${apiShellRcFile}
+  '';
 in {
   options = {
     vpsadmin.api = {
@@ -209,6 +231,10 @@ in {
         ip6tables -A nixos-fw -p tcp -m tcp -s ${ip} --dport ${toString port} -j nixos-fw-accept
       '')) cfg.allowedIPv6Ranges)
     ));
+
+    environment.systemPackages = [
+      apiShellScript
+    ];
 
     systemd.tmpfiles.rules = [
       "d '${cfg.stateDir}' 0750 ${cfg.user} ${cfg.group} - -"
