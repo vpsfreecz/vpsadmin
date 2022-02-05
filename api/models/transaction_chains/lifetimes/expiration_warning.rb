40f058a03e8a66f18603b1c23cc818355c704e80
Jakub Skokan <jakub.skokan@havefun.cz>
2022-02-05 18:27:18 +0100

    api: add helper variables to object expiration mail template

--- api/models/transaction_chains/lifetimes/expiration_warning.rb
+++ api/models/transaction_chains/lifetimes/expiration_warning.rb
@@ -3,17 +3,22 @@ module TransactionChains
     label 'Expiration'
     allow_empty
 
-    def link_chain(klass, q)
+    def link_chain(klass, objects)
       base_url = ::SysConfig.get(:webui, :base_url)
+      now = Time.now.utc
 
-      q.each do |obj|
-        user = if obj.is_a?(::User)
-                 obj
-               elsif obj.respond_to?(:user)
-                 obj.user
-               else
-                 fail "Unable to find an owner for #{obj} of class #{klass}"
-               end
+      objects.each do |obj|
+        user =
+          if obj.is_a?(::User)
+            obj
+          elsif obj.respond_to?(:user)
+            obj.user
+          else
+            fail "Unable to find an owner for #{obj} of class #{klass}"
+          end
+
+        days_before = (obj.expiration_date - now) / 60 / 60 / 24
+        days_after = (now - obj.expiration_date) / 60 / 60 / 24
 
         mail(:expiration_warning, {
           params: {
@@ -25,7 +30,10 @@ module TransactionChains
             base_url: base_url,
             object: obj,
             state: obj.current_object_state,
-            klass.name.underscore => obj
+            expires_in_days: days_before,
+            expired_days_ago: days_after,
+            expires_in_a_day: days_before >= 0 && days_before <= 1.5,
+            klass.name.underscore => obj,
           }
         }) if user.mailer_enabled
       end
