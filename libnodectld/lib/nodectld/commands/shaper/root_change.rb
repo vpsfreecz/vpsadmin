0e07f5c5b9afd279377ceac99637874c37c8ef38
Jakub Skokan <jakub.skokan@havefun.cz>
2022-08-19 10:45:22 +0200

    libnodectld: completely disable shaper by config option
    (cherry picked from commit c3f91fed29d03a40145179369757dcb87a005b4d)

--- libnodectld/lib/nodectld/commands/shaper/root_change.rb
+++ libnodectld/lib/nodectld/commands/shaper/root_change.rb
@@ -3,11 +3,15 @@ module NodeCtld
     handle 2012
 
     def exec
+      return ok unless $CFG.get(:shaper, :enable)
+
       Shaper.update_root(@max_tx, @max_rx)
       ok
     end
 
     def rollback
+      return ok unless $CFG.get(:shaper, :enable)
+
       Shaper.update_root(@original['max_tx'], @original['max_rx']) if @original
       ok
     end
--- libnodectld/lib/nodectld/network_interface.rb
+++ libnodectld/lib/nodectld/network_interface.rb
@@ -14,16 +14,18 @@ module NodeCtld
 
     def add_route(addr, prefix, v, register, shaper, via: nil)
       if register
-        Shaper.add_ip(
-          @vps_id,
-          netif: @name,
-          addr: addr,
-          prefix: prefix,
-          version: v,
-          class_id: shaper['class_id'],
-          max_tx: shaper['max_tx'],
-          max_rx: shaper['max_rx'],
-        )
+        if $CFG.get(:shaper, :enable)
+          Shaper.add_ip(
+            @vps_id,
+            netif: @name,
+            addr: addr,
+            prefix: prefix,
+            version: v,
+            class_id: shaper['class_id'],
+            max_tx: shaper['max_tx'],
+            max_rx: shaper['max_rx'],
+          )
+        end
         Firewall.accounting.reg_ip(addr, prefix, v)
       end
 
@@ -41,14 +43,16 @@ module NodeCtld
 
     def del_route(addr, prefix, v, unregister, shaper)
       if unregister
-        Shaper.remove_ip(
-          @vps_id,
-          netif: @name,
-          addr: addr,
-          prefix: prefix,
-          version: v,
-          class_id: shaper['class_id'],
-        )
+        if $CFG.get(:shaper, :enable)
+          Shaper.remove_ip(
+            @vps_id,
+            netif: @name,
+            addr: addr,
+            prefix: prefix,
+            version: v,
+            class_id: shaper['class_id'],
+          )
+        end
         Firewall.accounting.unreg_ip(addr, prefix, v)
       end
 
