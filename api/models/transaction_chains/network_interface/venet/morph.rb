8eeebb625a528f128363135d6413f086c0c5de19
Jakub Skokan <jakub.skokan@havefun.cz>
2022-08-20 21:19:21 +0200

    api: add hook for morphing network interfaces
    (cherry picked from commit 27cfa039a7468a718a773f61e3e1537547575f88)

--- api/models/transaction_chains/network_interface/venet/morph.rb
+++ api/models/transaction_chains/network_interface/venet/morph.rb
@@ -9,7 +9,17 @@ module TransactionChains
     # @param netif [::NetworkInterface]
     # @param target_netif_type [Symbol]
     def link_chain(netif, target_netif_type)
-      send(:"into_#{target_netif_type}", netif)
+      orig_kind = netif.kind
+
+      ret = send(:"into_#{target_netif_type}", netif)
+
+      ret.call_class_hooks_for(
+        :morph,
+        self,
+        args: [ret, orig_kind, ret.kind],
+      )
+
+      ret
     end
 
     protected
