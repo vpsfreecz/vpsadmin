08f502b2d45bd79e5faa1afd5f0aa1e6c51a8e87
Jakub Skokan <jakub.skokan@havefun.cz>
2022-09-13 16:08:20 +0200

    api: add pool status attributes to node status
    (cherry picked from commit 9908f7adc5b31a6f50ecd9965156005faf67eef0)

--- api/db/migrate/20220913114040_add_pool_status.rb
+++ api/db/migrate/20220913114040_add_pool_status.rb
@@ -3,5 +3,9 @@ class AddPoolStatus < ActiveRecord::Migration[6.1]
     add_column :pools, :state, :integer, null: false, default: 0
     add_column :pools, :scan, :integer, null: false, default: 0
     add_column :pools, :checked_at, :datetime, null: true
+
+    add_column :node_current_statuses, :pool_state, :integer, null: false, default: 0
+    add_column :node_current_statuses, :pool_scan, :integer, null: false, default: 0
+    add_column :node_current_statuses, :pool_checked_at, :datetime, null: true
   end
 end
