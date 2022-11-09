af802d0d020be3f79afa9b6afbd87a38bf29c602
Jakub Skokan <jakub.skokan@havefun.cz>
2022-11-09 19:50:38 +0100

    api: increase default VPS start timeout to 5min
    (cherry picked from commit 3215458d139518c92d5f484931d3ca47f81477e6)

--- api/models/transactions/vps/start.rb
+++ api/models/transactions/vps/start.rb
@@ -4,7 +4,7 @@ module Transactions::Vps
     t_type 1001
     queue :vps
 
-    def params(vps, start_timeout: 180)
+    def params(vps, start_timeout: 60*5)
       self.vps_id = vps.id
       self.node_id = vps.node_id
 
