d747df37fbba63960adc24af1423c63d3f608d1c
Jakub Skokan <jakub.skokan@havefun.cz>
2022-11-11 19:51:12 +0100

    libnodectld: handle pool import/export events and pause/resume nodectld

---
+++ libnodectld/lib/nodectld/pool_hook.rb
@@ -0,0 +1,26 @@
+module NodeCtld
+  # Interface for external hook scripts to communicate events to the nodectld
+  # daemon.
+  module PoolHook
+    def self.pre_import(pool)
+      RemoteClient.send_or_not(RemoteControl::SOCKET, :pool_hook, {
+        hook_name: :pre_import,
+        pool: pool,
+      })
+    end
+
+    def self.post_import(pool)
+      RemoteClient.send_or_not(RemoteControl::SOCKET, :pool_hook, {
+        hook_name: :post_import,
+        pool: pool,
+      })
+    end
+
+    def self.pre_export(pool)
+      RemoteClient.send_or_not(RemoteControl::SOCKET, :pool_hook, {
+        hook_name: :pre_export,
+        pool: pool,
+      })
+    end
+  end
+end
---
+++ libnodectld/lib/nodectld/remote_commands/pool_hook.rb
@@ -0,0 +1,36 @@
+require 'libosctl'
+
+module NodeCtld::RemoteCommands
+  class PoolHook < Base
+    handle :pool_hook
+
+    include OsCtl::Lib::Utils::Log
+
+    def exec
+      daemon = NodeCtld::Daemon.instance
+
+      case @hook_name.to_sym
+      when :pre_import
+        log(:info, 'pool-hook', "Pool #{@pool} is being imported, pausing")
+        daemon.node.pool_down(@pool)
+        daemon.pause
+
+      when :post_import
+        log(:info, 'pool-hook', "Pool #{@pool} has been imported")
+        daemon.node.pool_up(@pool)
+
+        if daemon.node.all_pools_up?
+          log(:info, 'pool-hook', 'All pools are now online, resuming')
+          daemon.resume
+        end
+
+      when :pre_export
+        log(:info, 'pool-hook', "Pool #{@pool} is being exported, pausing")
+        daemon.node.pool_down(@pool)
+        daemon.pause
+      end
+
+      ok
+    end
+  end
+end
