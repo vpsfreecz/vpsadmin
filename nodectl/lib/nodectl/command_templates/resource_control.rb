b69825421f23d631ba929379afd1348f2321b4f8
Jakub Skokan <jakub.skokan@havefun.cz>
2022-11-14 20:55:14 +0100

    nodectl: remove firewall-related commands

--- nodectl/lib/nodectl/command_templates/resource_control.rb
+++ nodectl/lib/nodectl/command_templates/resource_control.rb
@@ -1,26 +1,23 @@
 module NodeCtl
   class CommandTemplates::ResourceControl < Command::Remote
-    args 'all|fw|shaper'
+    args 'all|shaper'
 
     def validate
       if args.size < 1
         raise ValidationError, 'missing resource'
 
-      elsif !%w(all fw shaper).include?(args[0])
+      elsif !%w(all shaper).include?(args[0])
         raise ValidationError, 'not a valid resource'
       end
 
       params.update({
-        resources: args[0] == 'all' ? [:fw, :shaper] : [args[0]]
+        resources: args[0] == 'all' ? [:shaper] : [args[0]]
       })
     end
 
     def process
       response.each do |k, v|
         case k
-        when :fw
-          puts 'Firewall  ...  ok'
-
         when :shaper
           puts 'Shaper  ...  ok'
         end
