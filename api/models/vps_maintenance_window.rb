602969ee1653ba5156bfec811e950e3a024d277c
Jakub Skokan <jakub.skokan@havefun.cz>
2023-01-23 15:35:05 +0100

    api: move make_maintenance_windows from migration to model

--- api/models/vps_maintenance_window.rb
+++ api/models/vps_maintenance_window.rb
@@ -44,4 +44,82 @@ class VpsMaintenanceWindow < ActiveRecord::Base
       errors.add(:closes_at, 'maintenance window per week must be at least 12 hours long')
     end
   end
+
+  # Generate maintenance windows specific for this migration
+  #
+  # @param vps [::Vps]
+  # @param finish_weekday [Integer]
+  # @param finish_minutes [Integer]
+  # @return [Array<VpsMaintenanceWindow>] a list of temporary maintenance windows
+  def self.make_for(vps, finish_weekday:, finish_minutes:)
+    # The first open day is finish_weekday. Days after finish_weekday are
+    # completely open as well. Days until finish_weekday remain closed.
+    windows = (0..6).map do |i|
+      ::VpsMaintenanceWindow.new(
+        vps: vps,
+        weekday: i,
+      )
+    end
+
+    finish_day = windows[finish_weekday]
+    finish_day.assign_attributes(
+      is_open: true,
+      opens_at: finish_minutes,
+      closes_at: 24*60,
+    )
+
+    cur_day = Time.now.wday
+
+    if cur_day == finish_day.weekday
+      # The window is today, therefore all days are open
+      windows.each do |w|
+        next if w.weekday == finish_day.weekday
+
+        w.assign_attributes(
+          is_open: true,
+          opens_at: 0,
+          closes_at: 24*60,
+        )
+      end
+
+    else
+      7.times do |day|
+        next if day == finish_day.weekday
+
+        is_open =
+          if cur_day < finish_day.weekday
+            # The window opens later this week:
+            #  - the days before cur_day (next week) are open
+            #  - the days after finish_day this week are open
+            day < cur_day || day >= finish_day.weekday
+          else
+            # The window opens next week
+            #  - the days next week after finish_day but before cur_day are open
+            day >= finish_day.weekday && day < cur_day
+          end
+
+        if is_open
+          windows[day].assign_attributes(
+            is_open: true,
+            opens_at: 0,
+            closes_at: 24*60,
+          )
+        else
+          windows[day].assign_attributes(
+            is_open: false,
+            opens_at: nil,
+            closes_at: nil,
+          )
+        end
+      end
+    end
+
+    windows.delete_if { |w| !w.is_open }
+
+    unless windows.detect { |w| w.is_open }
+      fail 'programming error: no maintenance window is open'
+    end
+
+    windows
+  end
 end
