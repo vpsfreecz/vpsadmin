e9bd8fdcdf728621e2be1fb6422b5bdf54dd1297
Jakub Skokan <jakub.skokan@havefun.cz>
2024-01-24 15:35:22 +0100

    webui: require outage type to be selected

--- webui/forms/outage.forms.php
+++ webui/forms/outage.forms.php
@@ -76,7 +76,7 @@ function outage_report_form () {
 
 	$xtpl->form_add_input(_('Date and time').':', 'text', '30', 'begins_at', date('Y-m-d H:i'));
 	$xtpl->form_add_number(_('Duration').':', 'duration', post_val('duration'), 0, 999999, 1, 'minutes');
-	api_param_to_form('type', $input->type);
+	api_param_to_form('type', $input->type, null, null, true);
 	api_param_to_form('impact', $input->impact);
 
 	$entities = maintenance_to_entities();
