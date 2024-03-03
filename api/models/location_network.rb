35e95739ac0b996b1b53e3e80b3b69e609371fe5
Jakub Skokan <jakub.skokan@havefun.cz>
2024-03-03 14:48:25 +0100

    api: set LocationNetwork.primary to nil instead of false

--- api/models/location_network.rb
+++ api/models/location_network.rb
@@ -1,4 +1,14 @@
 class LocationNetwork < ::ActiveRecord::Base
   belongs_to :location
   belongs_to :network
+
+  # There's a unique index on primary, we want it to be set either to `true`
+  # or to `nil`, so that there could be multiple non-primary records.
+  before_save :nillify_primary
+
+  protected
+
+  def nillify_primary
+    self.primary = nil if primary === false
+  end
 end
