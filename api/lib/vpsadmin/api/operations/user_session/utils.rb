8dffc61b26921cba00cd17ab666faaebd597ece2
Jakub Skokan <jakub.skokan@havefun.cz>
2024-06-12 15:48:50 +0200

    api: always save client IP to user session record

--- api/lib/vpsadmin/api/operations/user_session/utils.rb
+++ api/lib/vpsadmin/api/operations/user_session/utils.rb
@@ -14,15 +14,21 @@ module VpsAdmin::API
     # @param admin [::User]
     # @return [::UserSession]
     def open_session(user:, request:, auth_type:, scope:, generate_token:, token_lifetime: 'fixed', token_interval: nil, admin: nil, label: nil)
+      api_ip_addr = request.ip
+      api_ip_ptr = get_ptr(api_ip_addr)
+
+      client_ip_addr = request.env['HTTP_CLIENT_IP'] || api_ip_addr
+      client_ip_ptr = client_ip_addr == api_ip_addr ? api_ip_ptr : get_ptr(client_ip_addr)
+
       user_session = ::UserSession.new(
         user:,
         admin:,
         auth_type:,
         scope:,
-        api_ip_addr: request.ip,
-        api_ip_ptr: get_ptr(request.ip),
-        client_ip_addr: request.env['HTTP_CLIENT_IP'],
-        client_ip_ptr: request.env['HTTP_CLIENT_IP'] && get_ptr(request.env['HTTP_CLIENT_IP']),
+        api_ip_addr:,
+        api_ip_ptr:,
+        client_ip_addr:,
+        client_ip_ptr:,
         user_agent: ::UserAgent.find_or_create!(request.user_agent || ''),
         client_version: request.user_agent || '',
         token_lifetime:,
