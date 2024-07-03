781073bf1c9041c7ce45fe148c2db38a962d3053
Jakub Skokan <jakub.skokan@havefun.cz>
2024-07-03 10:53:29 +0200

    api: fix deletion of location networks

--- api/lib/vpsadmin/api/operations/location_network/delete.rb
+++ api/lib/vpsadmin/api/operations/location_network/delete.rb
@@ -5,7 +5,7 @@ module VpsAdmin::API
     # @param ln [::LocationNetwork]
     def run(ln)
       ActiveRecord::Base.transaction do
-        ln.network.update!(location: nil) if ln.primary
+        ln.network.update!(primary_location: nil) if ln.primary
         ln.destroy!
       end
     end
