04e4ca9da0602a1be3fbe7f83c62dd1c18d59601
Jakub Skokan <jakub.skokan@havefun.cz>
2024-07-24 20:38:59 +0200

    webui: fix 00:00 maintenace window in migration confirmation form

--- webui/forms/vps.forms.php
+++ webui/forms/vps.forms.php
@@ -1200,9 +1200,11 @@ function vps_migrate_form_step3($vps_id, $node_id, $opts)
     $days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     $hours = [];
 
-    for ($i = 0; $i < 24; $i++) {
+    for ($i = 0; $i < 25; $i++) {
         $hours[] = sprintf("%02d:00", $i);
     }
+
+    $hours[ count($hours) - 1 ] = '23:59';
 
     if ($opts['maintenance_window'] == '1') {
         $windows_td = '<table>';
