8e797a800770d12fde8dfcb121d1c3aa7b0fb7d5
Jakub Skokan <jakub.skokan@havefun.cz>
2024-08-05 21:44:05 +0200

    libnodectld: set motd and shell prompt for VPS in rescue mode

--- libnodectld/lib/nodectld/commands/vps/boot.rb
+++ libnodectld/lib/nodectld/commands/vps/boot.rb
@@ -1,7 +1,7 @@
 module NodeCtld
   class Commands::Vps::Boot < Commands::Base
     handle 2029
-    needs :system, :osctl
+    needs :system, :osctl, :vps
 
     def exec
       boot_opts = {
@@ -18,6 +18,57 @@ module NodeCtld
       boot_opts[:mount_root_dataset] = @mount_root_dataset if @mount_root_dataset
 
       osctl(%i[ct boot], @vps_id, boot_opts)
+
+      boot_rootfs = osctl_parse(%i[ct show], [@vps_id])[:boot_rootfs]
+
+      fork_chroot_wait do
+        # Some systems setup /etc on the first boot, wait for them a bit
+        15.times do
+          break if File.exist?('/etc/profile')
+
+          sleep(1)
+        end
+
+        # Set login message
+        File.open('/etc/motd', 'w') do |f|
+          f.write(<<~END)
+
+
+            ********************************************************************************
+            *                        WARNING: VPS in rescue mode!                          *
+            ********************************************************************************
+
+            The rescue system is temporary and will be destroyed when the VPS
+            is rebooted or halted. Backup any data that you wish to preserve.
+          END
+
+          if @mount_root_dataset
+            f.write(<<~END)
+
+              Root file system of VPS #{@vps_id} is mounted to "#{@mount_root_dataset}".
+            END
+          end
+
+          f.write("\n\n")
+        end
+
+        # Customize shell prompt
+        ['/etc/profile', '/root/.profile'].each do |profile|
+          File.open(profile, 'a') do |f|
+            f.puts(<<~END)
+
+              # vpsAdmin rescue mode
+              PS1="[VPS in rescue mode]\n$PS1"
+            END
+          end
+
+          break
+        rescue SystemCallError
+          next
+        end
+      end
+
+      ok
     end
 
     def rollback
