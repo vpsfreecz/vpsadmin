vpsfreecz/vpsadmin

commit 90f44e9f76a648c0fce837a647c79a2c849e6f87

rubocop: use ENV.fetch

api/lib/vpsadmin/api/tasks/lifetimes.rb
@@ -21,11 +21,11 @@ module VpsAdmin::API::Tasks
       required_env(%w[OBJECTS])
 
       time = Time.now.utc
-      time -= ENV['GRACE'].to_i if ENV['GRACE']
+      time -= ENV.fetch('GRACE', 0).to_i
 
-      expiration = (Time.now.utc + ENV['NEW_EXPIRATION'].to_i if ENV['NEW_EXPIRATION'])
+      expiration = Time.now.utc + ENV.fetch('NEW_EXPIRATION', 0).to_i
 
-      limit = ENV['LIMIT'] ? ENV['LIMIT'].to_i : 30
+      limit = ENV.fetch('LIMIT', 30).to_i
       raise 'invalid limit' if limit <= 0
 
       puts "Progressing objects having expiration date older than #{time}"
@@ -156,7 +156,7 @@ module VpsAdmin::API::Tasks
 
       classes = []
 
-      ENV['OBJECTS'].split(',').each do |obj|
+      ENV.fetch('OBJECTS').split(',').each do |obj|
         cls = Object.const_get(obj)
 
         raise warn "Unable to find a class for '#{obj}'" unless obj
@@ -170,7 +170,7 @@ module VpsAdmin::API::Tasks
     def get_states
       return unless ENV['STATES']
 
-      ENV['STATES'].split(',').map do |v|
+      ENV.fetch('STATES').split(',').map do |v|
         i = VpsAdmin::API::Lifetimes::STATES.index(v.to_sym)
         next(i) if i
 
@@ -180,9 +180,9 @@ module VpsAdmin::API::Tasks
 
     def get_reason(obj)
       user = get_user(obj)
-      return ENV['REASON'] if user.nil?
+      return ENV.fetch('REASON', '') if user.nil?
 
-      ENV["REASON_#{user.language.code.upcase}"] || ENV['REASON']
+      ENV["REASON_#{user.language.code.upcase}"] || ENV.fetch('REASON', '')
     end
 
     def get_user(obj)

libnodectld/lib/nodectld/standalone.rb
@@ -7,7 +7,7 @@ if NodeCtld::STANDALONE
   require 'nodectld/config'
 
   OsCtl::Lib::Logger.setup(:stdout)
-  $CFG = NodeCtld::AppConfig.new(ENV['CONFIG'] || '/etc/vpsadmin/nodectld.yml')
+  $CFG = NodeCtld::AppConfig.new(ENV.fetch('CONFIG', '/etc/vpsadmin/nodectld.yml'))
 
-  exit(false) unless $CFG.load(ENV['DB_CONFIG'])
+  exit(false) unless $CFG.load(ENV.fetch('DB_CONFIG', false))
 end

libnodectld/libnodectld.gemspec
@@ -25,11 +25,11 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'filelock'
   s.add_runtime_dependency 'ipaddress', '~> 0.8.3'
   s.add_runtime_dependency 'json'
-  s.add_runtime_dependency 'libosctl', ENV['OS_BUILD_ID']
+  s.add_runtime_dependency 'libosctl', ENV.fetch('OS_BUILD_ID')
   s.add_runtime_dependency 'mail', '~> 2.8.1'
   s.add_runtime_dependency 'mysql2', '0.5.5'
-  s.add_runtime_dependency 'osctl', ENV['OS_BUILD_ID']
-  s.add_runtime_dependency 'osctl-exportfs', ENV['OS_BUILD_ID']
+  s.add_runtime_dependency 'osctl', ENV.fetch('OS_BUILD_ID')
+  s.add_runtime_dependency 'osctl-exportfs', ENV.fetch('OS_BUILD_ID')
   s.add_runtime_dependency 'prometheus-client', '~> 4.2.2'
   s.add_runtime_dependency 'pry', '~> 0.14.2'
   s.add_runtime_dependency 'pry-remote'

nodectl/lib/nodectl/commands/halt_reason.rb
@@ -178,7 +178,7 @@ module NodeCtl
     end
 
     def get_action_verb
-      case ENV['HALT_ACTION']
+      case ENV.fetch('HALT_ACTION')
       when 'halt', 'poweroff'
         'shutting down'
       when 'reboot'

nodectl/lib/nodectl/commands/incident_report.rb
@@ -281,8 +281,8 @@ module NodeCtl
     end
 
     def admin_headers
-      admin_id = ENV['VPSADMIN_USER_ID']
-      admin_name = ENV['VPSADMIN_USER_NAME']
+      admin_id = ENV.fetch('VPSADMIN_USER_ID', nil)
+      admin_name = ENV.fetch('VPSADMIN_USER_NAME', nil)
 
       if admin_id
         "Admin: #{admin_id} #{admin_name}"
@@ -297,8 +297,8 @@ module NodeCtl
       file.close
 
       loop do
-        unless Kernel.system(ENV['EDITOR'], file.path)
-          warn "#{ENV['EDITOR']} exited with non-zero status code"
+        unless Kernel.system(ENV.fetch('EDITOR', 'vim'), file.path)
+          warn "#{ENV.fetch('EDITOR', nil)} exited with non-zero status code"
           exit(false)
         end
 

nodectl/nodectl.gemspec
@@ -24,7 +24,7 @@ Gem::Specification.new do |s|
 
   s.add_runtime_dependency 'json'
   s.add_runtime_dependency 'libnodectld', s.version
-  s.add_runtime_dependency 'libosctl', ENV['OS_BUILD_ID']
+  s.add_runtime_dependency 'libosctl', ENV.fetch('OS_BUILD_ID')
   s.add_runtime_dependency 'pry', '~> 0.14.2'
   s.add_runtime_dependency 'pry-remote'
   s.add_runtime_dependency 'require_all', '~> 2.0.0'

plugins/payments/api/tasks/payments.rake
@@ -7,10 +7,10 @@ namespace :vpsadmin do
         exit(false)
       end
 
-      b = VpsAdmin::API::Plugins::Payments.get_backend(ENV['BACKEND'].strip.to_sym)
+      b = VpsAdmin::API::Plugins::Payments.get_backend(ENV.fetch('BACKEND').strip.to_sym)
 
       unless b
-        warn "BACKEND '#{ENV['BACKEND']}' not found"
+        warn "BACKEND '#{ENV.fetch('BACKEND')}' not found"
         exit(false)
       end
 

tasks/release.rb
@@ -109,8 +109,8 @@ namespace :vpsadmin do
           './tools/update_gem.sh',
           'packages',
           gem.to_s,
-          ENV['VPSADMIN_BUILD_ID'],
-          ENV['OS_BUILD_ID']
+          ENV.fetch('VPSADMIN_BUILD_ID'),
+          ENV.fetch('OS_BUILD_ID')
         )
 
         raise "unable to build #{gem}: failed with exit status #{$?.exitstatus}" unless ret

commit 2b9bd9d5bbfd1fe846156d393d0bc2fe94b8d794

api: fix expiration handling in rake vpsadmin:lifetimes:progress
If the NEW_EXPIRATION parameter is not provided, we must pass nil as
expiration and that will lead to default lifetime values being used.

Fixes: 90f44e9f76a648c0fce837a647c79a2c849e6f87

api/lib/vpsadmin/api/tasks/lifetimes.rb
@@ -23,7 +23,7 @@ module VpsAdmin::API::Tasks
       time = Time.now.utc
       time -= ENV.fetch('GRACE', 0).to_i
 
-      expiration = Time.now.utc + ENV.fetch('NEW_EXPIRATION', 0).to_i
+      expiration = Time.now.utc + ENV.fetch('NEW_EXPIRATION').to_i if ENV['NEW_EXPIRATION']
 
       limit = ENV.fetch('LIMIT', 30).to_i
       raise 'invalid limit' if limit <= 0