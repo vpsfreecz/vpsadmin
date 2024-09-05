f7d93bb89c88100a10778323d53cb19baa6fa8bc
Jakub Skokan <jakub.skokan@havefun.cz>
2024-09-05 17:21:29 +0200

    tasks/release: fix task vpsadmin:version
    Removed vpsadmind and vpsadmindctl files, updated mail_template version
    file path.

--- tasks/release.rb
+++ tasks/release.rb
@@ -8,15 +8,13 @@ namespace :vpsadmin do
     File.write('VERSION', "#{v}\n")
 
     [
-      'vpsadmind/lib/vpsadmind/version.rb',
-      'vpsadmindctl/lib/vpsadmindctl/version.rb',
       'libnodectld/lib/nodectld/version.rb',
       'nodectl/lib/nodectl/version.rb',
       'console_router/lib/vpsadmin/console_router/version.rb',
       'client/lib/vpsadmin/client/version.rb',
       'download_mounter/lib/vpsadmin/download_mounter/version.rb',
       'api/lib/vpsadmin/api/version.rb',
-      'mail_templates/lib/vpsadmin/mail-templates/version.rb'
+      'mail_templates/lib/vpsadmin/mail_templates/version.rb'
     ].each do |file|
       File.write(file, File.read(file).sub(/ VERSION = '[^']+'/, " VERSION = '#{v}'"))
     end
