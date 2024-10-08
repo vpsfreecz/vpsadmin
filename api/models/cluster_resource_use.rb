8bcf1fe27b4c5c7073b90c47aa5a56f933fd2c51
Jakub Skokan <jakub.skokan@havefun.cz>
2024-10-08 16:12:43 +0200

    api: rephrase resource allocation errors, humanize values

--- api/models/cluster_resource_use.rb
+++ api/models/cluster_resource_use.rb
@@ -1,6 +1,7 @@
 require_relative 'confirmable'
 
 class ClusterResourceUse < ApplicationRecord
+  include ActiveSupport::NumberHelper
   include Confirmable
 
   belongs_to :user_cluster_resource
@@ -47,28 +48,41 @@ class ClusterResourceUse < ApplicationRecord
               used - value_was + value
             end
 
-    name = user_cluster_resource.cluster_resource.name
+    label = user_cluster_resource.cluster_resource.label
     min = user_cluster_resource.cluster_resource.min
     max = user_cluster_resource.cluster_resource.max
 
+    max_use = if resource_transfer
+                user_cluster_resource.value - used
+              else
+                (value_was || value) + (user_cluster_resource.value - used)
+              end
+
     if !admin_override && total > user_cluster_resource.value && total > used
-      errors.add(
-        :value,
-        "cannot allocate more #{name} than is available (#{user_cluster_resource.value - used} left)"
-      )
+      msg = "you only have #{fmt_value(user_cluster_resource.value - used)} of #{label} in " \
+            "#{user_cluster_resource.environment.label} left. #{capitalize_first_letter(fmt_object_name)} " \
+            "can use at most #{fmt_value(max_use)} of #{label}. "
+
+      msg << if user_reconfigurable?
+               "Either reconfigure the #{fmt_class_name} or contact support if you need more resources."
+             else
+               'Contact support if you need more resources.'
+             end
+
+      errors.add(:value, msg)
     end
 
     if !admin_override && value > max
-      errors.add(:value, "cannot allocate more #{name} than #{max} to one object")
+      errors.add(:value, "one #{fmt_class_name} cannot use more than #{fmt_value(max)} of #{label} resource")
 
     elsif !admin_override && value < min
-      errors.add(:value, "cannot allocate less #{name} than #{min} to one object")
+      errors.add(:value, "one #{fmt_class_name} cannot use less than #{fmt_value(min)} of #{label} resource")
     end
 
     if (value % user_cluster_resource.cluster_resource.stepsize) != 0
       errors.add(
         :value,
-        "#{name} is not a multiple of step size (#{user_cluster_resource.cluster_resource.stepsize})"
+        "#{label} is not a multiple of step size (#{fmt_value(user_cluster_resource.cluster_resource.stepsize)})"
       )
     end
 
@@ -76,13 +90,57 @@ class ClusterResourceUse < ApplicationRecord
 
     case admin_lock_type
     when 'absolute'
-      errors.add(:value, "cannot allocate #{name} other than #{admin_limit}") if admin_limit != value
+      errors.add(:value, "you must use exactly #{fmt_value(admin_limit)} of #{label}") if admin_limit != value
 
     when 'not_less'
-      errors.add(:value, "cannot allocate less #{name} than #{admin_limit}") if value < admin_limit
+      errors.add(:value, "you must use more of #{label} than #{fmt_value(admin_limit)}") if value < admin_limit
 
     when 'not_more'
-      errors.add(:value, "cannot allocate more #{name} than #{admin_limit}") if value > admin_limit
+      errors.add(:value, "you must use less of #{label} than #{fmt_value(admin_limit)}") if value > admin_limit
     end
+  end
+
+  def user_reconfigurable?
+    %w[Vps DatasetInPool].include?(class_name)
+  end
+
+  def fmt_class_name
+    {
+      'Vps' => 'VPS',
+      'DatasetInPool' => 'dataset',
+      'EnvironmentUserConfig' => 'user'
+    }[class_name] || class_name
+  end
+
+  def fmt_object_name
+    return fmt_class_name if new_record?
+
+    case class_name
+    when 'Vps'
+      "VPS #{row_id}"
+    when 'DatasetInPool'
+      begin
+        "dataset #{::DatasetInPool.find(row_id).dataset.full_name}"
+      rescue ActiveRecord::RecordNotFound
+        "dataset #{row_id}"
+      end
+    when 'EnvironmentUserConfig'
+      "user #{row_id}"
+    else
+      class_name
+    end
+  end
+
+  def fmt_value(value)
+    case user_cluster_resource.cluster_resource.name
+    when 'diskspace', 'memory', 'swap'
+      number_to_human_size(value * 1024 * 1024)
+    else
+      value
+    end
+  end
+
+  def capitalize_first_letter(str)
+    str[0].upcase + str[1..]
   end
 end
