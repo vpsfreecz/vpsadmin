c2252f743650fa457a6433a6a7be29d6d8500533
Jakub Skokan <jakub.skokan@havefun.cz>
2025-02-27 15:21:02 +0100

    libnodectld: move code from osctl hooks into libnodectld
    Whenever osctl hooks need changing, e.g. when a new parameter is added,
    etc., it is necessary to regenerate all installed hook scripts. By
    moving all logic into libnodectld, the hook scripts can remain unchanged
    and updating libnodectl will be enough.

--- libnodectld/lib/nodectld/ct_hook.rb
+++ libnodectld/lib/nodectld/ct_hook.rb
@@ -2,20 +2,47 @@ module NodeCtld
   # Interface for external hook scripts to communicate events to the nodectld
   # daemon.
   module CtHook
-    def self.pre_start(pool, ct_id); end
-
-    def self.post_mount(pool, ct_id, rootfs_mount, map_mode, ns_pid)
-      Mounter.mount_vps_mounts(File.join(pool, 'ct'), ct_id, rootfs_mount, map_mode, ns_pid)
+    def self.pre_start(env)
+      require_vars!(env, %w[OSCTL_POOL_NAME OSCTL_CT_ID])
     end
 
-    def self.veth_up(pool, ct_id, host_veth, ct_veth)
+    def self.post_mount(env)
+      require_vars!(
+        env,
+        %w[OSCTL_POOL_NAME OSCTL_CT_ID OSCTL_CT_ROOTFS_MOUNT OSCTL_CT_MAP_MODE OSCTL_CT_NS_PID]
+      )
+
+      Mounter.mount_vps_mounts(
+        File.join(env['OSCTL_POOL_NAME'], 'ct'),
+        env['OSCTL_CT_ID'],
+        env['OSCTL_CT_ROOTFS_MOUNT'],
+        env['OSCTL_CT_MAP_MODE'],
+        env['OSCTL_CT_NS_PID'].to_i
+      )
+    end
+
+    def self.veth_up(env)
+      require_vars!(env, %w[OSCTL_POOL_NAME OSCTL_CT_ID OSCTL_HOST_VETH OSCTL_CT_VETH])
+
       RemoteClient.send_or_not(RemoteControl::SOCKET, :ct_hook, {
                                  hook_name: :veth_up,
-                                 pool:,
-                                 vps_id: ct_id.to_i,
-                                 host_veth:,
-                                 ct_veth:
+                                 pool: env['OSCTL_POOL_NAME'],
+                                 vps_id: env['OSCTL_CT_ID'].to_i,
+                                 host_veth: env['OSCTL_HOST_VETH'],
+                                 ct_veth: env['OSCTL_CT_VETH']
                                })
+    end
+
+    def self.require_vars!(env, vars)
+      vars.each do |v|
+        next if env[v]
+
+        warn 'Expected environment variables:'
+        warn "  #{vars.join("\n  ")}"
+        warn
+        warn "#{v} not found"
+        exit(false)
+      end
     end
   end
 end
--- libnodectld/lib/nodectld/pool_hook.rb
+++ libnodectld/lib/nodectld/pool_hook.rb
@@ -2,25 +2,43 @@ module NodeCtld
   # Interface for external hook scripts to communicate events to the nodectld
   # daemon.
   module PoolHook
-    def self.pre_import(pool)
+    def self.pre_import(env)
+      require_vars!(env, %w[OSCTL_POOL_NAME])
+
       RemoteClient.send_or_not(RemoteControl::SOCKET, :pool_hook, {
                                  hook_name: :pre_import,
-                                 pool:
+                                 pool: env['OSCTL_POOL_NAME']
                                })
     end
 
-    def self.post_import(pool)
+    def self.post_import(env)
+      require_vars!(env, %w[OSCTL_POOL_NAME])
+
       RemoteClient.send_or_not(RemoteControl::SOCKET, :pool_hook, {
                                  hook_name: :post_import,
-                                 pool:
+                                 pool: env['OSCTL_POOL_NAME']
                                })
     end
 
-    def self.pre_export(pool)
+    def self.pre_export(env)
+      require_vars!(env, %w[OSCTL_POOL_NAME])
+
       RemoteClient.send_or_not(RemoteControl::SOCKET, :pool_hook, {
                                  hook_name: :pre_export,
-                                 pool:
+                                 pool: env['OSCTL_POOL_NAME']
                                })
+    end
+
+    def self.require_vars!(env, vars)
+      vars.each do |v|
+        next if env[v]
+
+        warn 'Expected environment variables:'
+        warn "  #{vars.join("\n  ")}"
+        warn
+        warn "#{v} not found"
+        exit(false)
+      end
     end
   end
 end
