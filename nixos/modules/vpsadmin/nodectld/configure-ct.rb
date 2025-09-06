f6e209d98e94f32d6a0d345862bd029f03f3cb4d
Jakub Skokan <jakub.skokan@havefun.cz>
2025-09-06 21:32:56 +0200

    nixos: add ctstartmenu to managed VMs

---
+++ nixos/modules/vpsadmin/nodectld/configure-ct.rb
@@ -0,0 +1,43 @@
+#!@ruby@/bin/ruby
+require 'erb'
+require 'fileutils'
+require 'json'
+
+class ConfigureContainer
+  LXC_CONFIG = '@lxcConfig@'.freeze
+
+  CTSTARTMENU = '@ctstartmenu@/bin/ctstartmenu'.freeze
+
+  def self.run
+    new.run
+  end
+
+  def run
+    tpl = ERB.new(File.read(LXC_CONFIG), trim_mode: '-')
+    vars = {}
+
+    config =
+      begin
+        JSON.parse(File.read('/run/config/vpsadmin/config.json'))
+      rescue Errno::ENOENT, JSON::ParserError
+        {}
+      end
+
+    vars[:init_cmd] = config.fetch('init_cmd', '/sbin/init')
+    start_menu_timeout = config.fetch('start_menu_timeout', 5)
+
+    if start_menu_timeout > 0
+      FileUtils.mkdir_p('/dev/.vpsadmin')
+      FileUtils.cp(CTSTARTMENU, '/dev/.vpsadmin/ctstartmenu')
+      File.chmod(0o755, '/dev/.vpsadmin/ctstartmenu')
+      vars[:init_cmd] = "/dev/.vpsadmin/ctstartmenu -timeout #{start_menu_timeout} #{vars[:init_cmd]}"
+    end
+
+    result = tpl.result_with_hash(vars)
+
+    FileUtils.mkdir_p('/var/lib/lxc/vps')
+    File.write('/var/lib/lxc/vps/config', result)
+  end
+end
+
+ConfigureContainer.run
