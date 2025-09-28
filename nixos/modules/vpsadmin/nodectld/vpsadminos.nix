3d42acea3635bdedca1a607366012035a2b9fba5
Jakub Skokan <jakub.skokan@havefun.cz>
2025-09-28 18:32:59 +0200

    nixos: add option vpsadmin.nodectld.version

--- nixos/modules/vpsadmin/nodectld/vpsadminos.nix
+++ nixos/modules/vpsadmin/nodectld/vpsadminos.nix
@@ -8,6 +8,10 @@ with lib;
 let
   cfg = config.vpsadmin.nodectld;
 
+  packages = pkgs."nodectl-v${cfg.version}";
+
+  useLibvirt = cfg.version == "5";
+
   managedVm = import ./managed-vm.nix { inherit config pkgs lib; };
 in
 {
@@ -18,23 +22,45 @@ in
   config = mkIf cfg.enable {
     nixpkgs.overlays = import ../../../overlays;
 
+    boot.kernelModules = mkIf useLibvirt [
+      "nbd"
+    ];
+
     boot.postBootCommands = ''
-      mkdir -m 0700 /run/nodectl
+      mkdir -p -m 0711 /run/nodectl
       ln -sfn /run/current-system/sw/bin/nodectl /run/nodectl/nodectl
     '';
 
-    system.activationScripts.nodectl.text = ''
-      ln -sfn ${managedVm} /run/nodectl/managed-vm
+    system.activationScripts = mkIf useLibvirt {
+      nodectl.text = ''
+        mkdir -p -m 0711 /run/nodectl
+        ln -sfn ${managedVm} /run/nodectl/managed-vm
+      '';
+    };
+
+    virtualisation.libvirtd.qemu.verbatimConfig = mkIf useLibvirt ''
+      # Prevent managed-vm stage2/config drive to be chowned to the qemu user
+      # (not working since those are stored in the Nix store)
+      dynamic_ownership = 0
     '';
 
     runit.services.nodectld = {
+      path = mkIf useLibvirt (
+        with pkgs;
+        [
+          btrfs-progs
+          e2fsprogs
+          qemu
+          xorriso
+        ]
+      );
       run = ''
         ulimit -c unlimited
         export HOME=${config.users.extraUsers.root.home}
         export LANG=en_US.UTF-8
         export LOCALE_ARCHIVE=/run/current-system/sw/lib/locale/locale-archive
         exec 2>&1
-        exec ${pkgs.nodectld}/bin/nodectld --log syslog --log-facility local3
+        exec ${packages.nodectld}/bin/nodectld --log syslog --log-facility local3
       '';
       killMode = "process";
     };
@@ -56,11 +82,16 @@ in
       nodectl halt-reason >> "$HALT_REASON_FILE"
     '';
 
-    environment.systemPackages = with pkgs; [
-      mbuffer
-      nodectl
-      openssh
-      vmexec
-    ];
+    environment.systemPackages =
+      with pkgs;
+      [
+        mbuffer
+        openssh
+        packages.nodectl
+      ]
+      ++ optionals useLibvirt [
+        console_client
+        vmexec
+      ];
   };
 }
