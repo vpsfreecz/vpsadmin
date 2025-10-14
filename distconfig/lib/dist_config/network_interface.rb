948c0c82e38cb72b1072d5443f63ee8990eda8a1
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-14 17:15:59 +0200

    distconfig, libnodectld-v5: use fe80::1 as IPv6 gateway

--- distconfig/lib/dist_config/network_interface.rb
+++ distconfig/lib/dist_config/network_interface.rb
@@ -22,6 +22,8 @@ module DistConfig
 
     DEFAULT_IPV4 = '255.255.255.254'.freeze
 
+    DEFAULT_IPV6 = 'fe80::1'.freeze
+
     # @return [String]
     attr_reader :guest_name
 
@@ -69,31 +71,8 @@ module DistConfig
       when 4
         DEFAULT_IPV4
       when 6
-        host_link_local_address
+        DEFAULT_IPV6
       end
-    end
-
-    protected
-
-    def host_link_local_address
-      raise 'host MAC address must be set' if @host_mac.nil?
-
-      # Normalize MAC (strip separators, lowercase)
-      parts = @host_mac.downcase.split(/[:-]/).map { |p| p.to_i(16) }
-      raise ArgumentError, 'invalid MAC' unless parts.size == 6
-
-      # Flip the 7th bit (Universal/Local bit) of the first byte
-      parts[0] ^= 0x02
-
-      # Insert ff:fe in the middle (EUI-64 expansion)
-      eui64 = parts[0..2] + [0xff, 0xfe] + parts[3..5]
-
-      # Format as IPv6 hextets
-      hextets = []
-      eui64.each_slice(2) { |a, b| hextets << ((a << 8) | b) }
-
-      # Build the link-local address
-      "fe80::#{hextets.map { |h| h.to_s(16) }.join(':')}"
     end
   end
 end
