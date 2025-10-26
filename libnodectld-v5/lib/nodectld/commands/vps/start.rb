3beed3c2a7cc8b2caf7395cb8be1738f1d8d0222
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-26 09:18:27 +0100

    libnodectld-v5: add domain lookup helpers

--- libnodectld-v5/lib/nodectld/commands/vps/start.rb
+++ libnodectld-v5/lib/nodectld/commands/vps/start.rb
@@ -1,20 +1,16 @@
 module NodeCtld
   class Commands::Vps::Start < Commands::Base
     handle 1001
+    needs :vps
 
     def exec
-      conn = LibvirtClient.new
-      dom = conn.lookup_domain_by_uuid(@vps_uuid)
-      Vps.new(dom, cmd: self).start(autostart_priority: @autostart_priority)
+      vps.start(autostart_priority: @autostart_priority)
       ok
     end
 
     def rollback
       if @rollback_start
-        conn = LibvirtClient.new
-        dom = conn.lookup_domain_by_uuid(@vps_uuid)
-
-        Vps.new(dom, cmd: self).stop
+        vps.stop
       end
 
       ok
