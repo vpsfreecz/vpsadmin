91f6b26cb0326ac9fcb4e11b64477446caf8c40d
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-27 14:01:29 +0100

    distconfig: support for updating VPS config at runtime

--- distconfig/lib/dist_config/vps_config.rb
+++ distconfig/lib/dist_config/vps_config.rb
@@ -18,6 +18,7 @@ module DistConfig
     attr_reader :network_interfaces
 
     def initialize(path)
+      @path = path
       @cfg = JSON.parse(File.read(path))
 
       @distribution = @cfg.fetch('distribution')
@@ -25,6 +26,23 @@ module DistConfig
       @hostname = @cfg['hostname'] && Hostname.new(@cfg['hostname'])
       @dns_resolvers = @cfg.fetch('dns_resolvers', nil)
       @network_interfaces = @cfg.fetch('network_interfaces', []).map { |v| NetworkInterface.new(v) }
+    end
+
+    def dump
+      {
+        'distribution' => @distribution,
+        'version' => @version,
+        'hostname' => @hostname && @hostname.to_s,
+        'dns_resolvers' => @dns_resolvers,
+        'network_interfaces' => @network_interfaces.map(&:dump)
+      }
+    end
+
+    def save
+      tmp = "#{@path}.new"
+
+      File.write(tmp, dump.to_json)
+      File.rename(tmp, @path)
     end
   end
 end
