faf2f314fecd559a0315fce63d1a190bbae6f937
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-27 09:38:01 +0100

    api, libnodectld-v5: implement libvirt VPS destroy

--- api/models/transaction_chains/vps/soft_delete.rb
+++ api/models/transaction_chains/vps/soft_delete.rb
@@ -3,8 +3,14 @@ module TransactionChains
     label 'Soft delete'
 
     def link_chain(vps, target, _state, _log)
-      lock(vps.dataset_in_pool)
+      if vps.container?
+        lock(vps.dataset_in_pool)
+      else
+        lock(vps.storage_volume)
+      end
+
       lock(vps)
+
       concerns(:affect, [vps.class.name, vps.id])
 
       # Stop VPS - should be already stopped when suspended (blocked)
@@ -20,8 +26,14 @@ module TransactionChains
       append_t(Transactions::Utils::NoOp, args: vps.node_id) do |t|
         # Mark all resources as disabled until they are really freed by
         # hard_delete. Revive should mark them back as enabled.
-        objs = [vps, vps.dataset_in_pool]
-        objs.concat(vps.dataset_in_pool.subdatasets_in_pool)
+        objs = [vps]
+
+        if vps.container?
+          objs << vps.dataset_in_pool
+          objs.concat(vps.dataset_in_pool.subdatasets_in_pool)
+        else
+          objs << vps.storage_volume
+        end
 
         objs.each do |obj|
           lock(obj)
