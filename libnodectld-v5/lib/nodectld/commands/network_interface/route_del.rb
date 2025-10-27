4251356c36d832e9172f40d3bd9bd3586f2297a7
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-27 09:44:29 +0100

    api, libnodectld-v5: implement host IP/route management

--- libnodectld-v5/lib/nodectld/commands/network_interface/route_del.rb
+++ libnodectld-v5/lib/nodectld/commands/network_interface/route_del.rb
@@ -1,10 +1,10 @@
 module NodeCtld
   class Commands::NetworkInterface::RouteDel < Commands::Base
     handle 2007
-    needs :osctl, :routes
+    needs :libvirt, :routes, :vps
 
     def exec
-      NetworkInterface.new(@pool_fs, @vps_id, @veth_name).del_route(
+      NetworkInterface.new(domain, @host_name, @guest_name).del_route(
         @addr, @prefix, @version, @unregister
       )
       ok
@@ -12,7 +12,7 @@ module NodeCtld
 
     def rollback
       wait_for_route_to_clear(@version, @addr, @prefix, timeout: @timeout)
-      NetworkInterface.new(@pool_fs, @vps_id, @veth_name).add_route(
+      NetworkInterface.new(domain, @host_name, @guest_name).add_route(
         @addr, @prefix, @version, @unregister, via: @via
       )
       ok
--- libnodectld-v5/lib/nodectld/network_interface.rb
+++ libnodectld-v5/lib/nodectld/network_interface.rb
@@ -4,51 +4,61 @@ module NodeCtld
   class NetworkInterface
     include OsCtl::Lib::Utils::Log
     include Utils::System
-    include Utils::OsCtl
+    include Utils::Libvirt
 
-    def initialize(pool_fs, vps_id, name)
-      @pool_fs = pool_fs
-      @vps_id = vps_id
-      @name = name
+    def initialize(domain, host_name, guest_name)
+      @domain = domain
+      @vps_id = domain.name
+      @host_name = host_name
+      @guest_name = guest_name
     end
 
-    def add_route(addr, prefix, _v, _register, via: nil)
-      VpsConfig.edit(@pool_fs, @vps_id) do |cfg|
-        cfg.network_interfaces[@name].add_route(config_route(
-                                                  addr, prefix, via
-                                                ))
+    def add_route(addr, prefix, v, _register, via: nil)
+      VpsConfig.edit(@vps_id) do |cfg|
+        cfg.network_interfaces[@guest_name].add_route(config_route(addr, prefix, via))
+        ConfigDrive.create(@vps_id, cfg)
       end
 
-      opts = {}
-      opts[:via] = via if via
+      return unless @domain.active?
 
-      osctl(%i[ct netif route add], [@vps_id, @name, "#{addr}/#{prefix}"], opts)
+      if via
+        syscmd("ip -#{v} route add #{addr}/#{prefix} via #{via} dev #{@host_name}")
+      else
+        syscmd("ip -#{v} route add #{addr}/#{prefix} dev #{@host_name}")
+      end
     end
 
-    def del_route(addr, prefix, _v, _unregister)
-      VpsConfig.edit(@pool_fs, @vps_id) do |cfg|
-        cfg.network_interfaces[@name].remove_route(config_route(
-                                                     addr, prefix, nil
-                                                   ))
+    def del_route(addr, prefix, v, _unregister)
+      VpsConfig.edit(@vps_id) do |cfg|
+        cfg.network_interfaces[@guest_name].remove_route(config_route(addr, prefix, nil))
+        ConfigDrive.create(@vps_id, cfg)
       end
 
-      osctl(%i[ct netif route del], [@vps_id, @name, "#{addr}/#{prefix}"])
+      return unless @domain.active?
+
+      syscmd("ip -#{v} route del #{addr}/#{prefix} dev #{@host_name}")
     end
 
-    def add_host_addr(addr, prefix)
-      osctl(
-        %i[ct netif ip add],
-        [@vps_id, @name, "#{addr}/#{prefix}"],
-        { no_route: true }
-      )
+    def add_host_addr(addr, prefix, v)
+      VpsConfig.edit(@vps_id) do |cfg|
+        cfg.network_interfaces[@guest_name].add_ip(v, "#{addr}/#{prefix}")
+        ConfigDrive.create(@vps_id, cfg)
+      end
+
+      return unless @domain.active?
+
+      distconfig!(@domain, ['hostaddr-add', @guest_name, addr, prefix])
     end
 
-    def del_host_addr(addr, prefix)
-      osctl(
-        %i[ct netif ip del],
-        [@vps_id, @name, "#{addr}/#{prefix}"],
-        { keep_route: true }
-      )
+    def del_host_addr(addr, prefix, v)
+      VpsConfig.edit(@vps_id) do |cfg|
+        cfg.network_interfaces[@guest_name].remove_ip(v, "#{addr}/#{prefix}")
+        ConfigDrive.create(@vps_id, cfg)
+      end
+
+      return unless @domain.active?
+
+      distconfig!(@domain, ['hostaddr-del', @guest_name, addr, prefix])
     end
 
     protected
