a62abdee66e0e4682830daf69e0b408f1176845f
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-27 09:44:29 +0100

    api, libnodectld-v5: implement host IP/route management

--- libnodectld-v5/lib/nodectld/commands/network_interface/route_add.rb
+++ libnodectld-v5/lib/nodectld/commands/network_interface/route_add.rb
@@ -1,18 +1,18 @@
 module NodeCtld
   class Commands::NetworkInterface::RouteAdd < Commands::Base
     handle 2006
-    needs :routes
+    needs :libvirt, :routes, :vps
 
     def exec
       wait_for_route_to_clear(@version, @addr, @prefix, timeout: @timeout)
-      NetworkInterface.new(@pool_fs, @vps_id, @veth_name).add_route(
+      NetworkInterface.new(domain, @host_name, @guest_name).add_route(
         @addr, @prefix, @version, @register, via: @via
       )
       ok
     end
 
     def rollback
-      NetworkInterface.new(@pool_fs, @vps_id, @veth_name).del_route(
+      NetworkInterface.new(domain, @host_name, @guest_name).del_route(
         @addr, @prefix, @version, @register
       )
       ok
