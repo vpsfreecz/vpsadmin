b9b3a8f7ece5dc1154c36a6114630a69781f8146
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-27 09:44:29 +0100

    api, libnodectld-v5: implement host IP/route management

--- api/models/transactions/network_interface/del_route.rb
+++ api/models/transactions/network_interface/del_route.rb
@@ -12,17 +12,32 @@ module Transactions::NetworkInterface
       self.vps_id = netif.vps.id
       self.node_id = netif.vps.node_id
 
-      {
-        pool_fs: (pool || netif.vps.dataset_in_pool.pool).filesystem,
-        veth_name: netif.name,
-        addr: ip.addr,
-        prefix: ip.prefix,
-        version: ip.version,
-        via: ip.route_via && ip.route_via.ip_addr,
-        unregister:,
-        id: ip.id,
-        user_id: ip.user_id || netif.vps.user_id
-      }
+      if netif.vps.container?
+        {
+          pool_fs: (pool || netif.vps.dataset_in_pool.pool).filesystem,
+          veth_name: netif.name,
+          addr: ip.addr,
+          prefix: ip.prefix,
+          version: ip.version,
+          via: ip.route_via && ip.route_via.ip_addr,
+          unregister:,
+          id: ip.id,
+          user_id: ip.user_id || netif.vps.user_id
+        }
+      else
+        {
+          vps_uuid: netif.vps.uuid.uuid,
+          host_name: netif.host_name,
+          guest_name: netif.guest_name,
+          addr: ip.addr,
+          prefix: ip.prefix,
+          version: ip.version,
+          via: via && via.ip_addr,
+          register:,
+          id: ip.id,
+          user_id: ip.user_id || netif.vps.user_id
+        }
+      end
     end
   end
 end
--- libnodectld-v5/lib/nodectld/commands/network_interface/host_ip_add.rb
+++ libnodectld-v5/lib/nodectld/commands/network_interface/host_ip_add.rb
@@ -1,14 +1,15 @@
 module NodeCtld
   class Commands::NetworkInterface::HostIpAdd < Commands::Base
     handle 2022
+    needs :libvirt, :vps
 
     def exec
-      NetworkInterface.new(@pool_fs, @vps_id, @interface).add_host_addr(@addr, @prefix)
+      NetworkInterface.new(domain, @host_name, @guest_name).add_host_addr(@addr, @prefix, @version)
       ok
     end
 
     def rollback
-      NetworkInterface.new(@pool_fs, @vps_id, @interface).del_host_addr(@addr, @prefix)
+      NetworkInterface.new(domain, @host_name, @guest_name).del_host_addr(@addr, @prefix, @version)
       ok
     end
   end
