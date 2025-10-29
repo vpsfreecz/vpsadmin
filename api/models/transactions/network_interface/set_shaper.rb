a618d1044574a99a42a2c21c496e01dcf3a79ad3
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-29 14:52:02 +0100

    api, libnodectld-v5: support for shaper configuration on libvirt VPS

--- api/models/transactions/network_interface/set_shaper.rb
+++ api/models/transactions/network_interface/set_shaper.rb
@@ -10,18 +10,34 @@ module Transactions::NetworkInterface
       self.vps_id = vps.id
       self.node_id = vps.node_id
 
-      {
-        pool_fs: vps.dataset_in_pool.pool.filesystem,
-        veth_name: name || netif.name,
-        max_tx: max_tx && {
-          new: max_tx,
-          original: netif.max_tx
-        },
-        max_rx: max_rx && {
-          new: max_rx,
-          original: netif.max_rx
+      if vps.container?
+        {
+          pool_fs: vps.dataset_in_pool.pool.filesystem,
+          veth_name: name || netif.name,
+          max_tx: max_tx && {
+            new: max_tx,
+            original: netif.max_tx
+          },
+          max_rx: max_rx && {
+            new: max_rx,
+            original: netif.max_rx
+          }
         }
-      }
+      else
+        {
+          vps_uuid: vps.uuid.to_s,
+          host_name: netif.host_name,
+          guest_name: netif.guest_name,
+          max_tx: {
+            new: max_tx || netif.max_tx,
+            original: netif.max_tx
+          },
+          max_rx: {
+            new: max_rx || netif.max_rx,
+            original: netif.max_rx
+          }
+        }
+      end
     end
   end
 end
