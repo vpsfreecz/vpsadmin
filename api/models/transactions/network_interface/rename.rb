403cff537856919b826c4e82727aebb796bd081d
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-29 16:29:41 +0100

    api, distconfig, libnodectld-v5: rename network interfaces of libvirt VPS

--- api/models/transactions/network_interface/rename.rb
+++ api/models/transactions/network_interface/rename.rb
@@ -8,12 +8,22 @@ module Transactions::NetworkInterface
       self.vps_id = netif.vps.id
       self.node_id = netif.vps.node_id
 
-      {
-        pool_fs: netif.vps.dataset_in_pool.pool.filesystem,
-        name: new_name,
-        original: orig,
-        netif_id: netif.id
-      }
+      if netif.vps.container?
+        {
+          pool_fs: netif.vps.dataset_in_pool.pool.filesystem,
+          name: new_name,
+          original: orig,
+          netif_id: netif.id
+        }
+      else
+        {
+          vps_uuid: netif.vps.uuid.to_s,
+          host_name: netif.host_name,
+          guest_name: orig,
+          new_guest_name: new_name,
+          netif_id: netif.id
+        }
+      end
     end
   end
 end
