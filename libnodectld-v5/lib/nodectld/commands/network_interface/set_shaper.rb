1774d23b4d2aaa6c1503fde44e6aa172cc6fc481
Jakub Skokan <jakub.skokan@havefun.cz>
2025-10-29 14:52:02 +0100

    api, libnodectld-v5: support for shaper configuration on libvirt VPS

--- libnodectld-v5/lib/nodectld/commands/network_interface/set_shaper.rb
+++ libnodectld-v5/lib/nodectld/commands/network_interface/set_shaper.rb
@@ -1,31 +1,15 @@
 module NodeCtld
   class Commands::NetworkInterface::SetShaper < Commands::Base
     handle 2031
-    needs :system, :osctl, :vps
+    needs :system, :libvirt, :vps
 
     def exec
-      osctl(
-        %i[ct netif set],
-        [@vps_id, @veth_name],
-        {
-          max_tx: @max_tx && @max_tx['new'],
-          max_rx: @max_rx && @max_rx['new']
-        }.compact
-      )
-
+      NetworkInterface.new(domain, @host_name, @guest_name).set_shaper(@max_tx['new'], @max_rx['new'])
       ok
     end
 
     def rollback
-      osctl(
-        %i[ct netif set],
-        [@vps_id, @veth_name],
-        {
-          max_tx: @max_tx && @max_tx['original'],
-          max_rx: @max_rx && @max_rx['original']
-        }.compact
-      )
-
+      NetworkInterface.new(domain, @host_name, @guest_name).set_shaper(@max_tx['original'], @max_rx['original'])
       ok
     end
   end
