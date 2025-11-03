2e8a9230b59c8df4a6eb42b941198e0587ffc50d
Jakub Skokan <jakub.skokan@havefun.cz>
2025-11-03 12:36:15 +0100

    api: add OS family name

--- api/models/os_family.rb
+++ api/models/os_family.rb
@@ -3,5 +3,5 @@ class OsFamily < ApplicationRecord
   has_many :os_templates, dependent: :restrict_with_exception
   has_many :vpses, dependent: :restrict_with_exception
 
-  validates :label, presence: true
+  validates :name, :label, presence: true
 end
