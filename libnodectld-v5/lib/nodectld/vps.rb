158cfbff708217ff64ac586abbe4d79fc72f3126
Jakub Skokan <jakub.skokan@havefun.cz>
2025-11-03 19:11:34 +0100

    libnodectld-v5: support passwd/stop on user-controlled libvirt VPS

--- libnodectld-v5/lib/nodectld/vps.rb
+++ libnodectld-v5/lib/nodectld/vps.rb
@@ -10,9 +10,10 @@ module NodeCtld
     include Utils::System
     include Utils::Libvirt
 
-    def initialize(domain, cmd: nil)
+    def initialize(domain, vps_config: nil, cmd: nil)
       @domain = domain
       @vps_id = domain.name
+      @vps_config = vps_config || VpsConfig.read(@vps_id)
       @cmd = cmd
     end
 
@@ -21,8 +22,57 @@ module NodeCtld
     end
 
     def stop(kill: false, timeout: 300)
+      if @vps_config.vm_type == 'qemu_container'
+        stop_qemu_container(kill:, timeout:)
+      else
+        stop_qemu_full(kill:, timeout:)
+      end
+    end
+
+    def restart(autostart_priority: nil)
+      stop
+      start(autostart_priority:)
+    end
+
+    def passwd(user, password)
+      if @vps_config.vm_type == 'qemu_container'
+        distconfig!(@domain, %W[passwd #{user}], input: password)
+      else
+        @domain.qemu_agent_command({
+          'execute' => 'guest-set-user-password',
+          'arguments' => {
+            'username' => user,
+            'password' => password
+          }
+        }.to_json)
+      end
+    end
+
+    def honor_state
+      before = @domain.active?
+      yield
+      after = @domain.active?
+
+      if before && !after
+        start
+      elsif !before && after
+        stop
+      end
+    end
+
+    def log_type
+      if @cmd
+        @cmd.log_type
+      else
+        "vps=#{@vps_id}"
+      end
+    end
+
+    protected
+
+    def stop_qemu_container(kill:, timeout:)
       begin
-        st, = distconfig(@domain, %W[stop #{kill ? 'kill' : 'stop'} #{timeout}])
+        st, = distconfig(@domain, ['stop', kill ? 'kill' : 'stop', timeout])
       rescue Libvirt::Error => e
         log(:warn, "Error during graceful shutdown of VPS #{@vps_id}: #{e.message} (#{e.class})")
       end
@@ -45,33 +95,32 @@ module NodeCtld
       nil
     end
 
-    def restart(autostart_priority: nil)
-      stop
-      start(autostart_priority:)
-    end
-
-    def passwd(user, password)
-      distconfig!(@domain, %W[passwd #{user}], input: password)
-    end
-
-    def honor_state
-      before = @domain.active?
-      yield
-      after = @domain.active?
-
-      if before && !after
-        start
-      elsif !before && after
-        stop
+    def stop_qemu_full(kill:, timeout:)
+      if kill
+        @domain.destroy
+        return
       end
-    end
 
-    def log_type
-      if @cmd
-        @cmd.log_type
-      else
-        "vps=#{@vps_id}"
+      begin
+        @domain.shutdown(Libvirt::Domain::SHUTDOWN_GUEST_AGENT)
+      rescue Libvirt::Error => e
+        log(:warn, "Unable to shutdown VPS #{@vps_id} using guest agent: #{e.message} (#{e.class})")
       end
+
+      timeout.times do
+        return unless @domain.active?
+
+        log(:debug, "Waiting for VPS #{@vps_id} to gracefully shutdown")
+        sleep(1)
+      end
+
+      begin
+        @domain.destroy
+      rescue Libvirt::Error
+        # pass
+      end
+
+      nil
     end
   end
 end
