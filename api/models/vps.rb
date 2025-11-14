ca919d7a7977f81c943219041e7df0f9c9d24952
Jakub Skokan <jakub.skokan@havefun.cz>
2025-11-14 17:35:04 +0100

    api: log VPS IO stats in time

--- api/models/vps.rb
+++ api/models/vps.rb
@@ -38,7 +38,8 @@ class Vps < ApplicationRecord
 
   belongs_to :storage_volume
   has_many :storage_volumes
-  has_many :vps_io_stats
+  has_many :vps_io_stats, dependent: :delete_all
+  has_many :vps_io_stat_logs, dependent: :delete_all
 
   belongs_to :dataset_in_pool
   has_one :dataset, through: :dataset_in_pool, autosave: false
