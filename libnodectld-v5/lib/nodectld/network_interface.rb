d0832e12b2dd8e80c6ea4abdb3038e325165cee3
Jakub Skokan <jakub.skokan@havefun.cz>
2025-11-19 15:51:12 +0100

    libnodectld-v5: use distconfig mode when renaming network interface

--- libnodectld-v5/lib/nodectld/network_interface.rb
+++ libnodectld-v5/lib/nodectld/network_interface.rb
@@ -64,14 +64,12 @@ module NodeCtld
     end
 
     def rename(new_guest_name)
+      distconfig!(@domain, ['netif-rename', @guest_name, new_guest_name], run: true)
+
       VpsConfig.edit(@vps_id) do |cfg|
         cfg.network_interfaces.rename(@guest_name, new_guest_name)
         ConfigDrive.create(@vps_id, cfg)
       end
-
-      return unless @domain.active?
-
-      distconfig!(@domain, ['netif-rename', @guest_name, new_guest_name])
     end
 
     def add_route(addr, prefix, v, _register, via: nil)
