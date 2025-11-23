09fcb2a3cc3aba4bce1cca548758fe480a4a721c
Jakub Skokan <jakub.skokan@havefun.cz>
2025-11-23 18:10:30 +0100

    api, libnodectld-v5: implement user data for libvirt VPS
    cloud-init configuration is deployed using the NoCloud data source same
    as before. It was thought that we'll be able to unify cloud-init
    configuration for QEMU containers and full QEMUs and provide user data
    using filesystem with CIDATA label. However, cloud-init detects that
    QEMU container is using LXC and does not try to mount the filesystem
    even though it could -- it assumes that containers cannot mount.
    
    QEMU containers will therefore have their configuration delivered
    through the local filesystem at /var/lib/cloud/seed/nocloud and full
    QEMUs will use the config drive.

--- libnodectld-v5/lib/nodectld/commands/vps/deploy_user_data.rb
+++ libnodectld-v5/lib/nodectld/commands/vps/deploy_user_data.rb
@@ -1,14 +1,32 @@
 module NodeCtld
   class Commands::Vps::DeployUserData < Commands::Base
     handle 2035
+    needs :libvirt, :vps
 
     def exec
-      VpsUserData.for_format(@format).deploy(
-        @vps_id,
-        @format,
-        @content,
-        @os_template
-      )
+      case @format
+      when 'script'
+        active_or_distconfig_mode(domain) do
+          distconfig!(domain, %w[user-script-install], input: @content)
+        end
+
+      when 'cloudinit_config', 'cloudinit_script'
+        active_or_distconfig_mode(domain) do
+          distconfig!(domain, %w[start])
+          distconfig!(domain, %w[network-setup])
+          distconfig!(domain, %w[cloud-init-install], timeout: 30 * 60)
+          distconfig!(domain, ['cloud-init-deploy', @format], input: @content)
+        end
+      end
+
+      VpsConfig.edit(@vps_id) do |cfg|
+        cfg.user_data =
+          if @format.start_with?('cloudinit_')
+            VpsConfig::UserData.new(format: @format, content: @content)
+          end
+
+        ConfigDrive.create(@vps_id, cfg)
+      end
 
       ok
     end
