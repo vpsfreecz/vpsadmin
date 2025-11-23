0061324a25de5df9b7f47dcfe659b115994644ec
Jakub Skokan <jakub.skokan@havefun.cz>
2025-11-23 16:35:28 +0100

    distconfig: add nixos-config-apply command

--- distconfig/lib/dist_config/distributions/nixos.rb
+++ distconfig/lib/dist_config/distributions/nixos.rb
@@ -49,6 +49,33 @@ module DistConfig
       super if ct.impermanence.nil? || ct.running?
     end
 
+    def apply_nixos_config(format, content)
+      files = {
+        'nixos_configuration' => 'configuration.nix',
+        'nixos_flake_configuration' => 'flake.nix'
+      }
+
+      file = files[format]
+
+      if file
+        with_rootfs do
+          FileUtils.mkdir_p(nixos_config_dir)
+          File.write(File.join(nixos_config_dir, file), content)
+        end
+      end
+
+      wair_for_systemctl_running
+
+      script = <<~END
+        . /etc/profile
+        #{nixos_rebuild_command(format, content)} 2>&1 | tee /var/log/vpsadmin-nixos-output.log
+      END
+
+      ct_syscmd(['sh'], stdin: script)
+
+      nil
+    end
+
     def bin_path
       # TODO: this might not work with impermanence
       with_rootfs do
@@ -56,6 +83,38 @@ module DistConfig
       rescue Errno::ENOENT
         '/bin'
       end
+    end
+
+    protected
+
+    def wait_for_systemctl_running
+      3.times do
+        begin
+          out = ct_syscmd(['systemctl', 'is-system-running', '--wait']).output
+          return if out.strip == 'running'
+        rescue SystemCommandFailed
+          # pass
+        end
+
+        sleep(10)
+      end
+    end
+
+    def nixos_rebuild_command(format, content)
+      case format
+      when 'nixos_configuration'
+        "NIXOS_CONFIG=#{File.join(nixos_config_dir, 'configuration.nix')} nixos-rebuild switch"
+
+      when 'nixos_flake_configuration'
+        "nixos-rebuild switch --flake #{File.join(nixos_config_dir)}#vps"
+
+      when 'nixos_flake_uri'
+        "nixos-rebuild switch --flake \"#{content.strip}\""
+      end
+    end
+
+    def nixos_config_dir
+      '/etc/vpsadmin-nixos'
     end
   end
 end
