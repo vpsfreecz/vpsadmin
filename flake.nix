81ec1a1b274dfbd4a77e75e8947ce83b632d88cf
Jakub Skokan <jakub.skokan@havefun.cz>
2026-03-01 16:29:39 +0100

    flake: add dev shell instead of shell.nix

--- flake.nix
+++ flake.nix
@@ -85,5 +85,47 @@
       packages = forAllSystems (system: {
         test-runner = vpsadminos.packages.${system}.test-runner;
       });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ composedOverlay ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bundix
+              git
+              libffi
+              ncurses
+              ruby
+              zlib
+              mariadb
+              mariadb-connector-c
+              nixfmt-rfc-style
+              nixfmt-tree
+              php83Packages.php-cs-fixer
+            ];
+
+            shellHook = ''
+              export GEM_HOME="$(pwd)/.gems"
+              export RUBOCOP_CACHE_ROOT="$(pwd)/.rubocop_cache"
+              export PATH="$(ruby -e 'puts Gem.bindir'):$PATH"
+              export RUBYLIB="$GEM_HOME"
+              export PS1="(vpsadmin-dev) $PS1"
+              gem install --no-document bundler
+
+              # Purity disabled because of prism gem, which has a native extension.
+              # The extension has its header files in .gems, which gets stripped but
+              # cc wrapper in Nix. Without NIX_ENFORCE_PURITY=0, we get prism.h not found
+              # error.
+              NIX_ENFORCE_PURITY=0 bundle install
+            '';
+          };
+        }
+      );
     };
 }
