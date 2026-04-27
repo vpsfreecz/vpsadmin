bd854ee7ccded63e9853b24f8f847529504dfcf1
Jakub Skokan <jakub.skokan@havefun.cz>
2026-04-27 17:02:49 +0200

    libnodectld: cover user namespace commands
    Add native command specs for user namespace use and disuse.
    They verify exec and rollback call osctl user management correctly.

---
+++ libnodectld/spec/nodectld/commands/user_namespace/use_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'nodectld/commands/base'
+require 'nodectld/commands/user_namespace/use'
+
+RSpec.describe NodeCtld::Commands::UserNamespace::Use do
+  let(:driver) { build_storage_driver }
+  let(:cmd) do
+    described_class.new(
+      driver,
+      'pool_fs' => 'tank/ct',
+      'vps_id' => 101,
+      'name' => '42',
+      'uidmap' => ['0:100000:65536'],
+      'gidmap' => ['0:100000:65536']
+    )
+  end
+
+  before do
+    stub_const('NodeCtld::OsCtlUsers', Class.new)
+    allow(NodeCtld::OsCtlUsers).to receive_messages(
+      add_vps: { ret: :ok },
+      remove_vps: { ret: :ok }
+    )
+  end
+
+  it 'adds the osctl user on exec and removes it on rollback' do
+    expect(cmd.exec).to eq(ret: :ok)
+    expect(NodeCtld::OsCtlUsers).to have_received(:add_vps).with(
+      pool_fs: 'tank/ct',
+      vps_id: 101,
+      user_name: '42',
+      uidmap: ['0:100000:65536'],
+      gidmap: ['0:100000:65536']
+    )
+
+    expect(cmd.rollback).to eq(ret: :ok)
+    expect(NodeCtld::OsCtlUsers).to have_received(:remove_vps).with(
+      pool_fs: 'tank/ct',
+      vps_id: 101,
+      user_name: '42'
+    )
+  end
+end
