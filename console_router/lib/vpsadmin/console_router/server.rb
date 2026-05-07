16f894889629060a9194e22f790e1310190380a1
Jakub Skokan <jakub.skokan@havefun.cz>
2026-05-07 22:03:25 +0200

    console-router: lazy initialize router
    Delay creation of the RabbitMQ-backed router until the first request.
    This avoids opening broker connections while loading the Rack app and
    lets specs inject a fake router or factory before any request is served.
    
    Use a factory object instead of a Sinatra proc setting because Sinatra
    evaluates proc settings when they are read.

--- console_router/lib/vpsadmin/console_router/server.rb
+++ console_router/lib/vpsadmin/console_router/server.rb
@@ -1,20 +1,36 @@
 module VpsAdmin::ConsoleRouter
   class Server < Sinatra::Application
+    class RouterFactory
+      def call
+        Router.new
+      end
+    end
+
     configure do
       path = File.join(__dir__, '..', '..', '..')
 
       set :protection, except: :frame_options
       set :views, File.join(path, 'views')
       set :public_folder, File.join(path, 'public')
-      set :router, Router.new
+      set :router, nil
+      set :router_factory, RouterFactory.new
+    end
+
+    helpers do
+      def router
+        return settings.router if settings.router
+
+        settings.set(:router, settings.router_factory.call)
+        settings.router
+      end
     end
 
     get '/console/:vps_id' do |vps_id_str|
       vps_id = vps_id_str.to_i
 
-      if settings.router.check_session(vps_id, params[:session])
+      if router.check_session(vps_id, params[:session])
         erb :console, locals: {
-          api_url: settings.router.api_url,
+          api_url: router.api_url,
           vps_id:,
           auth_type: params[:auth_type],
           auth_token: params[:auth_token],
@@ -26,7 +42,7 @@ module VpsAdmin::ConsoleRouter
     end
 
     post '/console/feed/:vps_id' do |vps_id_str|
-      data = settings.router.read_write_console(
+      data = router.read_write_console(
         vps_id_str.to_i,
         params[:session],
         params[:keys],
