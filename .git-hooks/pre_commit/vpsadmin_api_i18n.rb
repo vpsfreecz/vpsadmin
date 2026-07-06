4c6d0dc01803a9e0f1b7c6739c0e2d00060d7358
Jakub Skokan <jakub.skokan@havefun.cz>
2026-07-06 10:50:18 +0200

    hooks: isolate API i18n bundle
    The API i18n pre-commit hook runs from the repository root, but invokes
    the API bundle. Clear the root Ruby and Bundler environment and use
    Bundler.with_unbundled_env before launching the API health task so the
    hook works reliably from the default vpsAdmin development shell.

--- .git-hooks/pre_commit/vpsadmin_api_i18n.rb
+++ .git-hooks/pre_commit/vpsadmin_api_i18n.rb
@@ -1,7 +1,26 @@
 module Overcommit::Hook::PreCommit
   class VpsadminApiI18n < Base
     def run
-      output = `cd api && env -u BUNDLE_BIN_PATH -u BUNDLE_GEMFILE -u BUNDLER_VERSION BUNDLE_GEMFILE=Gemfile BUNDLE_PATH=.gems bundle exec rake vpsadmin:i18n:health 2>&1`
+      command = [
+        'cd api && env',
+        '-u BUNDLE_BIN_PATH',
+        '-u BUNDLE_GEMFILE',
+        '-u BUNDLER_VERSION',
+        '-u GEM_HOME',
+        '-u GEM_PATH',
+        '-u RUBYLIB',
+        '-u RUBYOPT',
+        'BUNDLE_GEMFILE=Gemfile',
+        'BUNDLE_PATH=.gems',
+        'bundle exec rake vpsadmin:i18n:health'
+      ].join(' ')
+
+      output =
+        if defined?(Bundler)
+          Bundler.with_unbundled_env { `#{command} 2>&1` }
+        else
+          `#{command} 2>&1`
+        end
 
       return :pass if $?.exitstatus == 0
 
