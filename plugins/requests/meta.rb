93163ff338cd16c43c1f73dc5c4fb07714a04266
Jakub Skokan <jakub.skokan@havefun.cz>
2026-07-08 17:45:26 +0200

    notifications: make request candidates optional defaults
    Request notifications can choose type- and state-specific template ids at
    runtime, but the generic shipped templates remain the required defaults.

--- plugins/requests/meta.rb
+++ plugins/requests/meta.rb
@@ -30,23 +30,36 @@ VpsAdmin::API::Plugin.register(:requests) do
         NotificationTemplate.register(:"request_#{action}_#{audience}", vars:)
 
         %w[registration change].each do |type|
-          NotificationTemplate.register(:"request_#{action}_#{audience}_#{type}", vars:)
+          NotificationTemplate.register(
+            :"request_#{action}_#{audience}_#{type}",
+            vars:,
+            default: false
+          )
         end
       end
 
       NotificationTemplate.register(:"request_resolve_#{audience}", vars:)
 
       %w[registration change].each do |type|
-        NotificationTemplate.register(:"request_resolve_#{audience}_#{type}", vars:)
+        NotificationTemplate.register(
+          :"request_resolve_#{audience}_#{type}",
+          vars:,
+          default: false
+        )
       end
 
       %w[awaiting approved denied ignored pending_correction].each do |state|
-        NotificationTemplate.register(:"request_resolve_#{audience}_#{state}", vars:)
+        NotificationTemplate.register(
+          :"request_resolve_#{audience}_#{state}",
+          vars:,
+          default: false
+        )
 
         %w[registration change].each do |type|
           NotificationTemplate.register(
             :"request_resolve_#{audience}_#{type}_#{state}",
-            vars:
+            vars:,
+            default: false
           )
         end
       end
